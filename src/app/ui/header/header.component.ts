c77facc53c0c16f57a1f03325178a001054e71fc
Péter <peter.siket-szasz@csc.fi>
2019-07-12 09:47:16 +0300

    Refactored header component to be more accessible. Removed focusable outlines unless tabbing

--- src/app/ui/header/header.component.ts
+++ src/app/ui/header/header.component.ts
@@ -5,8 +5,7 @@
 // # :author: CSC - IT Center for Science Ltd., Espoo Finland servicedesk@csc.fi
 // # :license: MIT
 
-import { Component, OnInit } from '@angular/core';
-import { Title } from '@angular/platform-browser';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -14,6 +13,13 @@ import { Title } from '@angular/platform-browser';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  @ViewChild('mainNavbar') mainNavbar: ElementRef;
+  @ViewChild('navbarToggler') navbarToggler: ElementRef;
+
+  tabEventHandler: any;
+  mouseEventHandler: any;
+  escapeHandler: any;
+
   navbarOpen = false;
 
   mobile = window.innerWidth < 1217;
@@ -23,21 +29,54 @@ export class HeaderComponent implements OnInit {
 
   lang = 'fi';
 
+  constructor() { }
+
+  ngOnInit() {
+    this.tabEventHandler = this.handleTabPressed.bind(this);
+    this.mouseEventHandler = this.handleMouseDown.bind(this);
+    this.escapeHandler = this.escapeListener.bind(this);
+    window.addEventListener('keydown', this.tabEventHandler);
+    window.addEventListener('keydown', this.escapeHandler);
+  }
+
+  // Toggle between viewing and hiding focused element outlines
+  handleTabPressed(e) {
+    if (e.keyCode === 9) {
+      document.body.classList.add('user-tabbing');
+
+      window.removeEventListener('keydown', this.tabEventHandler);
+      window.addEventListener('mousedown', this.mouseEventHandler);
+    }
+  }
+
+  handleMouseDown() {
+    document.body.classList.remove('user-tabbing');
+
+    window.removeEventListener('mousedown', this.mouseEventHandler);
+    window.addEventListener('keydown', this.tabEventHandler);
+  }
+
+  escapeListener(e) {
+    if (e.keyCode === 27) {
+      this.toggleNavbar();
+      this.navbarToggler.nativeElement.focus();
+    }
+  }
+
   toggleNavbar() {
     this.navbarOpen = !this.navbarOpen;
-    const elem = document.getElementById('main-navbar');
 
     // First change display: 'block' so animation works, then change position after a small timeout
     if (this.navbarOpen) {
-      elem.style.display = 'block';
+      this.mainNavbar.nativeElement.style.cssText = 'display:block !important';
 
       setTimeout(() => {
-        elem.style.right = '0';
+        this.mainNavbar.nativeElement.style.right = '0';
       }, 10);
 
     } else {
-      elem.style.display = 'none';
-      elem.style.right = '-100%';
+      this.mainNavbar.nativeElement.style.cssText = 'display:none !important';
+      this.mainNavbar.nativeElement.style.right = '-100%';
     }
   }
 
@@ -51,15 +90,9 @@ export class HeaderComponent implements OnInit {
     if (this.width >= 1217) {
       this.mobile = false;
       if (this.navbarOpen) { this.toggleNavbar(); }
+      this.mainNavbar.nativeElement.style.cssText = '';
     } else {
       this.mobile = true;
     }
   }
-
-  constructor() { }
-
-
-  ngOnInit() {
-  }
-
 }
