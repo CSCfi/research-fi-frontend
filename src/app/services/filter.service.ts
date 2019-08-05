0c12ccd94096ec21cd6beb108ded1bdf2797c0c1
Sauli Purhonen <sauli.purhonen@visma.com>
2019-08-05 09:15:23 +0300

    Removed unnecessary variable

--- src/app/services/filter.service.ts
+++ src/app/services/filter.service.ts
@@ -21,7 +21,6 @@ const API_URL = environment.apiUrl;
 })
 export class FilterService {
   sort: string;
-  sortMethod: string;
   fromPage: number;
   requestCheck: boolean;
   singleInput: any;
@@ -35,7 +34,6 @@ export class FilterService {
 
   constructor( private searchService: SearchService, private http: HttpClient) {
     this.sort = this.searchService.sort;
-    this.sortMethod = this.searchService.sortMethod;
     this.fromPage = this.searchService.fromPage;
     this.requestCheck = this.searchService.requestCheck;
     this.singleInput = this.searchService.singleInput;
