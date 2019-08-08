0a654e8937ce3234428327a04f122c3583af7f0d
Sauli Purhonen <sauli.purhonen@visma.com>
2019-08-08 10:21:31 +0300

    Added switch statement to check tab and declare what filters are expected

--- src/app/component/results/active-filters/active-filters.component.ts
+++ src/app/component/results/active-filters/active-filters.component.ts
@@ -7,7 +7,8 @@
 
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { isArray, isString } from 'util';
+import { isArray } from 'util';
+import { SearchService } from '../../../services/search.service';
 
 @Component({
   selector: 'app-active-filters',
@@ -22,17 +23,22 @@ export class ActiveFiltersComponent implements OnInit, OnDestroy {
   status: any;
   combinedFilters: any;
   field: any;
+  currentTab: string;
+  filterParams: any;
 
-  constructor( private route: ActivatedRoute, private router: Router ) {
+  constructor( private route: ActivatedRoute, private router: Router, private searchService: SearchService ) {
     this.filter = [];
    }
 
   ngOnInit() {
+    this.currentTab = this.searchService.currentTab;
+
     this.queryParams = this.route.queryParams.subscribe(params => {
       this.filter = [params.year, params.status, params.field];
       this.year = params.year;
       this.status = params.status;
       this.field = params.field;
+      // console.log(this.filter);
 
       // If single filter, modify to array
       if (!isArray(this.year)) {this.year = [params.year]; }
@@ -50,21 +56,22 @@ export class ActiveFiltersComponent implements OnInit, OnDestroy {
 
   removeFilter(event): void {
     const yearParams = this.year.filter(e => e !== event.target.id);
-    let statusParams = this.status.filter(e => e !== event.target.id);
-    let fieldParams = this.field.filter(e => e !== event.target.id);
+    const statusParams = this.status.filter(e => e !== event.target.id);
+    const fieldParams = this.field.filter(e => e !== event.target.id);
 
-    // Handle undefined filters
-    if (statusParams) {statusParams = []; }
-    if (fieldParams) {fieldParams = []; }
+    // Remove filters according to tab
+    switch (this.currentTab) {
+      case 'publications': {
+        this.filterParams = {queryParams: {year: yearParams, field: fieldParams}, queryParamsHandling: 'merge' };
+        break;
+      }
+      case 'fundings': {
+        this.filterParams = {queryParams: {status: statusParams, year: yearParams} };
+        break;
+      }
+    }
 
-    this.router.navigate([], {
-      queryParams: {
-        year: yearParams,
-        status: statusParams,
-        field: fieldParams
-      },
-      queryParamsHandling: 'merge'
-    });
+    this.router.navigate([],this.filterParams);
   }
 
   ngOnDestroy() {
