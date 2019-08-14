39f09bd37782562346e0ef1586ae1a856ef98467
Péter <peter.siket-szasz@csc.fi>
2019-08-14 08:41:58 +0300

    Dummy component with fundings

--- src/app/component/results/all-results/all-results.component.ts
+++ src/app/component/results/all-results/all-results.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, ViewChild, ElementRef, Injector, Input, ComponentRef } from '@angular/core';
 import { ComponentPortal, DomPortalHost } from '@angular/cdk/portal';
 import { createDomPortalHost } from './utils';
+import { SearchService } from 'src/app/services/search.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-all-results',
@@ -14,30 +16,37 @@ export class AllResultsComponent implements OnInit {
   portalHost: DomPortalHost;
   @ViewChild('portalHost') elRef: ElementRef;
   components = [ChildOne, ChildTwo, ChildThree];
+  data = {};
 
-  constructor(readonly injector: Injector) { }
+  constructor(readonly injector: Injector, private searchService: SearchService, private http: HttpClient) { }
 
   ngOnInit() {
     this.portalHost = createDomPortalHost(this.elRef, this.injector);
+    const payload = {size: 10};
+    this.http.get(this.searchService.apiUrl + 'funding/_search?size=10').subscribe(data => {
+      this.data = data;
+    });
   }
 
   onClickAddRandomChild() {
-    const randomChild = this.components[Math.floor(Math.random() * this.components.length)];
+    const randomChild = ChildOne;
     const myPortal = new ComponentPortal(randomChild);
-    this.portalHost.detach()
+    this.portalHost.detach();
     const componentRef = this.portalHost.attach(myPortal);
     setTimeout(() => {
-      componentRef.instance.myInput = 'hello';
+      console.log(this.data);
+      componentRef.instance.fundingData = [this.data];
     }, 1);
   }
 }
 
 @Component({
   selector: 'app-child-one',
-  template: `<p>I am child one. <strong>{{myInput}}</strong></p>`
+  templateUrl: '../fundings/fundings.component.html'
 })
 export class ChildOne {
-  @Input() myInput = '';
+  @Input() fundingData;
+  expandStatus: Array<boolean> = [];
 }
 
 @Component({
