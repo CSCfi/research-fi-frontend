3c7dac3bb1dfeafd4f6d82d7605b1f80ba1abf78
Timo Särkikoski <sarkikos@gmail.com>
2019-08-22 07:24:41 +0300

    Modified environment files, removed hard coded 'http' from apiUrl.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -10,11 +10,11 @@ Copyright 2019 Ministry of Education and Culture, Finland
 /*
 For production:
 - Copy this file as 'environment.researchfi.prod.ts'.
-- Replace string '<API_HOST>' with the correct API server address.
+- Replace string '<API_HOST>' with the correct API server address including 'http' or 'https', for example, to 'http://localhost:8080'.
 */
 
 export const environment = {
   production: true,
-  apiUrl: 'http://<API_HOST>/portalapi/',
+  apiUrl: '<API_HOST>/portalapi/',
   buildInfo: '<BUILD_INFO>'
 };
