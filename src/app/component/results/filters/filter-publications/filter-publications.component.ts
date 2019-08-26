9cbecfe227c30cb55dcf3292440badbe31d05d72
Sauli Purhonen <sauli.purhonen@visma.com>
2019-08-26 13:43:15 +0300

    Search open access aggregation for codes and build new array where fallback values are sorted

--- src/app/component/results/filters/filter-publications/filter-publications.component.ts
+++ src/app/component/results/filters/filter-publications/filter-publications.component.ts
@@ -29,13 +29,15 @@ export class FilterPublicationsComponent implements OnInit, OnDestroy, OnChanges
   mobile = this.width < 992;
   @ViewChild('selectedYears') selectedYears: MatSelectionList;
   @ViewChildren('selectedFields') selectedFields: QueryList<MatSelectionList>;
-  @ViewChild('selectedOpenaccess') selectedOpenaccess: MatSelectionList;
+  @ViewChild('selectedOpenAccess') selectedOpenAccess: MatSelectionList;
   @ViewChild('filterSidebar') filterSidebar: ElementRef;
   preSelection = [];
 
   private resizeSub: Subscription;
+  filterSub: Subscription;
   yearFilters: any[];
   fieldOfScienceFilter: any;
+  openAccessFilter: any;
   fields: any;
   filterTerm: string;
 
@@ -61,7 +63,7 @@ export class FilterPublicationsComponent implements OnInit, OnDestroy, OnChanges
   mappedFieldsofScience: any;
   combinedFields: any[];
   mergedFields: any;
-  filterSub: Subscription;
+  openAccessCodes: any[];
   internationalCollab: boolean;
 
   constructor( private router: Router, private filterService: FilterService, private resizeService: ResizeService,
@@ -91,7 +93,7 @@ export class FilterPublicationsComponent implements OnInit, OnDestroy, OnChanges
     this.getSelected();
     this.router.navigate([],
     { queryParams: { page: 1, sort: this.sortService.sortMethod, year: this.yearFilters, field: this.fieldOfScienceFilter,
-      internationalCollaboration: this.internationalCollab } });
+      openAccess: this.openAccessFilter, internationalCollaboration: this.internationalCollab } });
   }
 
   // Select all from major
@@ -118,8 +120,9 @@ export class FilterPublicationsComponent implements OnInit, OnDestroy, OnChanges
 
   getSelected() {
     // If international collaboration is false, prevent param initalization
-    if(!this.internationalCollab) {this.internationalCollab = null; }
+    if (!this.internationalCollab) {this.internationalCollab = null; }
     this.yearFilters = this.selectedYears.selectedOptions.selected.map(s => s.value);
+    this.openAccessFilter = this.selectedOpenAccess.selectedOptions.selected.map(s => s.value);
 
     // Get minor fields of science from multiple selection lists
     const mergedFields = [];
@@ -156,6 +159,7 @@ export class FilterPublicationsComponent implements OnInit, OnDestroy, OnChanges
     const source = this.responseData[0] ? this.responseData[0].aggregations._index.buckets.publications.fieldsOfScience.buckets : [];
     this.fields = this.subFilter(source, this.filterTerm);
     this.separateMinor(source);
+    this.openAccess();
     this.isChecked();
   }
 
@@ -191,6 +195,22 @@ export class FilterPublicationsComponent implements OnInit, OnDestroy, OnChanges
       if (this.mappedFieldsofScience) {
         this.combinedFields.push(this.mappedFieldsofScience.filter(obj => obj.id.toString().charAt(0).includes(i)));
       }
+    }
+  }
+
+  // Open access
+  openAccess() {
+    const combined = [];
+    // Get aggregation from response
+    const source = this.responseData[0] ? this.responseData[0].aggregations._index.buckets.publications.openAccess.buckets : [];
+    if (source && source .length > 0) {
+      source.forEach(val => combined.push(val.key));
+      this.openAccessCodes = [];
+      // Check for matching access codes. -1 & 9 are fallbacks from old data
+      if (combined.includes(-1) || combined.includes(0) || combined.includes(9)) {this.openAccessCodes.push(
+        {key: 0, label: 'Ei vastausta', value: 'noAccessInfo'}); }
+      if (combined.includes(1)) {this.openAccessCodes.push({key: 1, label: 'Open access', value: 'openAccess'}); }
+      if (combined.includes(2)) {this.openAccessCodes.push({key: 2, label: 'Hybridijulkaisu', value: 'hybridAccess'}); }
     }
   }
 
