67976e3f9c549da502a095b1a94898e605f218f0
Sauli Purhonen <sauli.purhonen@visma.com>
2019-08-28 12:54:37 +0300

    Sort functionality for column headers

--- src/app/component/results/publications/publications.component.ts
+++ src/app/component/results/publications/publications.component.ts
@@ -5,14 +5,119 @@
 //  :author: CSC - IT Center for Science Ltd., Espoo Finland servicedesk@csc.fi
 //  :license: MIT
 
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { SortService } from '../../../services/sort.service';
 
 @Component({
   selector: 'app-publications',
   templateUrl: './publications.component.html',
   styleUrls: ['./publications.component.scss']
 })
-export class PublicationsComponent {
+export class PublicationsComponent implements OnInit {
   @Input() resultData: any [];
   expandStatus: Array<boolean> = [];
+  sortIndicator: any;
+
+  constructor(private router: Router, private route: ActivatedRoute, private sortService: SortService) { }
+
+  ngOnInit() {
+    this.addSortIndicator();
+  }
+
+  addSortIndicator() {
+    this.sortIndicator = [];
+    let sortMethod = this.sortService.sortMethod;
+    if (!sortMethod) {sortMethod = 'yearDesc'; }
+    switch (sortMethod) {
+      case 'name':
+      case 'author':
+      case 'journal':
+      case 'year': {
+        this.sortIndicator.push(sortMethod, 'asc');
+        break;
+      }
+      case 'nameDesc':
+      case 'authorDesc':
+      case 'journalDesc':
+      case 'yearDesc': {
+        this.sortIndicator.push(sortMethod, 'desc');
+        break;
+      }
+    }
+  }
+
+  sortBy(sortBy) {
+    const activeSort = this.route.snapshot.queryParams.sort;
+    let newSort: any;
+    switch (sortBy) {
+      case 'name': {
+        switch (activeSort) {
+          case 'name': {
+            newSort = 'nameDesc';
+            break;
+          }
+          case 'nameDesc': {
+            newSort = [];
+            break;
+          }
+          default: {newSort = 'name'; }
+        }
+        break;
+      }
+      case 'author': {
+        switch (activeSort) {
+          case 'author': {
+            newSort = 'authorDesc';
+            break;
+          }
+          case 'authorDesc': {
+            newSort = [];
+            break;
+          }
+          default: {newSort = 'author'; }
+        }
+        break;
+      }
+      case 'journal': {
+        switch (activeSort) {
+          case 'journal': {
+            newSort = 'journalDesc';
+            break;
+          }
+          case 'journalDesc': {
+            newSort = [];
+            break;
+          }
+          default: {newSort = 'journal'; }
+        }
+        break;
+      }
+      case 'year': {
+        switch (activeSort) {
+          case 'year': {
+            newSort = 'yearAsc';
+            this.sortIndicator = 'desc';
+            break;
+          }
+          case 'yearDesc': {
+            newSort = [];
+            break;
+          }
+          default: {newSort = 'year'; }
+        }
+        break;
+      }
+      default: {
+        newSort = 'yearDesc';
+      }
+    }
+    this.router.navigate([],
+      {
+        relativeTo: this.route,
+        queryParams: { sort: newSort },
+        queryParamsHandling: 'merge'
+      }
+    );
+  }
 }
