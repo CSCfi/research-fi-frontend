694ce18b41f49c568cf89ea16ab9a6275ed4189f
Sauli Purhonen <sauli.purhonen@visma.com>
2019-09-03 10:30:31 +0300

    Created auto suggest service to test queries with prefixes and mappings

--- src/app/services/autosuggest.service.ts
+++ src/app/services/autosuggest.service.ts
@@ -6,31 +6,114 @@
 //  :license: MIT
 
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import { Observable } from 'rxjs/Observable';
-import { debounceTime, distinctUntilChanged, switchMap, map } from 'rxjs/operators';
 
 const API_URL = environment.apiUrl;
 
 @Injectable()
 export class AutosuggestService {
-    apiUrl = API_URL;
+  apiUrl = API_URL;
 
-    constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) { }
 
-    search(terms: Observable<string>) {
-      return terms.pipe(
-        debounceTime(400),
-        distinctUntilChanged(),
-        switchMap(term => this.searchEntries(term)));
-    }
+  search(terms: Observable<string>) {
+    const payLoad = {
+			query: {
+				bool: {
+					should: [
+						{
+							bool: {
+								must: [
+									{ term: { _index: 'publication'	}	},
+									{	bool:
+										{	should: [
+											{ match_phrase_prefix: { publicationName: { query: terms } } }
+										]	}
+									}
+								]
+							}
+						},
+						{
+							bool: {
+								must: [
+									{ term: { _index: 'funding'	}	},
+									{	bool:
+										{	should: [
+											{ prefix: { projectNameFi: { value: terms } } }
+										]	}
+									}
+								]
+							}
+						},
+						{
+							bool: {
+								must: [
+									{ term: { _index: 'person'	}	},
+									{	bool:
+										{	should: [
+											{ prefix: { firstNames: { value: terms } } },
+											{ prefix: { lastName: { value: terms } } }
+										]	}
+									}
+								]
+							}
+						},
+						{
+							bool: {
+								must: [
+									{ term: { _index: 'organization'	}	},
+									{	bool:
+										{	should: [
+											{ prefix: { nameFi: { value: terms } } }
+										]	}
+									}
+								]
+							}
+						}
+					],
+					boost: 1
+				}
+			},
+			aggs: {
+					_index: {
+							filters: {
+									filters: {
+											person: {
+													match: {
+															_index: 'person'
+													}
+											},
+											publication: {
+													match: {
+															_index: 'publication'
+													}
+											},
+											funding: {
+													match: {
+															_index: 'funding'
+													}
+											},
+											organization: {
+													match: {
+															_index: 'organization'
+													}
+											}
+									}
+							},
+							aggs: {
+									index_results: {
+											top_hits: {
+													size: 3
+											}
+									}
+							}
+					}
+			}
+	};
 
-    searchEntries(term) {
-        console.log('term:', term)
-      return this.http
-          .get(this.apiUrl + 'publication,person,funding,organization/_search?' + term)
-          .pipe(map(res => res));
-    }
+	   return this.http.post(this.apiUrl + 'publication,person,funding,organization/_search?filter_path=aggregations&q=' + terms, payLoad);
+	}
 
 }
