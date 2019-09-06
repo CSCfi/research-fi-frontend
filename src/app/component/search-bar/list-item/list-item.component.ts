a8836898cb63998162b2b23c4547667fb3f99760
Sauli Purhonen <sauli.purhonen@visma.com>
2019-09-06 15:57:07 +0300

    Styling for list-items, list-item component

---
+++ src/app/component/search-bar/list-item/list-item.component.ts
@@ -0,0 +1,35 @@
+import { Component, HostBinding, Input } from '@angular/core';
+import { Highlightable } from '@angular/cdk/a11y';
+
+@Component({
+  selector: 'app-list-item',
+  template: `<div [class.disabled]="disabled">
+            <ng-content></ng-content>
+            </div>`
+})
+export class ListItemComponent implements Highlightable {
+  @Input() id;
+  @Input() doc;
+  @Input() term;
+  @Input() historyItem;
+  @Input() clear;
+  @Input() empty;
+  @Input() disabled = false;
+  private activeStatus = false;
+
+  @HostBinding('class.active') get isActive() {
+    return this.activeStatus;
+  }
+
+  setActiveStyles() {
+    this.activeStatus = true;
+  }
+
+  setInactiveStyles() {
+    this.activeStatus = false;
+  }
+
+  getLabel() {
+    return this.id + this.doc + this.term + this.historyItem + this.clear + this.empty;
+  }
+}
