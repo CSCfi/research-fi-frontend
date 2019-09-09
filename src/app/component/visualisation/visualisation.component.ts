7e02c42170058d1e7f58a33659458d26d72066d0
Péter Siket-Szász <peter.siketszasz@gmail.com>
2019-09-09 10:16:35 +0300

    Added search term to visualisation navigation

--- src/app/component/visualisation/visualisation.component.ts
+++ src/app/component/visualisation/visualisation.component.ts
@@ -206,7 +206,7 @@ export class VisualisationComponent implements OnInit, OnDestroy {
     const filterArray: string[] = p.ancestors().reverse().slice(1).map(d => d.data.key);
     const queryParam = {};
     this.hierarchy.forEach((e, i) => queryParam[e.queryField] = filterArray[i]);
-    this.router.navigate(['results/', this.index], {queryParams: queryParam});
+    this.router.navigate(['results/', this.index + 's', this.searchTerm], {queryParams: queryParam});
   }
 
   clicked(p) {
