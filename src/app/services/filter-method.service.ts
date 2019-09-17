d407cbf782b084023a32c2690a36f219c087c360
Sauli Purhonen <sauli.purhonen@visma.com>
2019-09-17 11:44:32 +0300

    Major & minor fields of study filtering logic

--- src/app/services/filter-method.service.ts
+++ src/app/services/filter-method.service.ts
@@ -4,6 +4,17 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class FilterMethodService {
+  majorFieldsOfScience = [
+    {fieldId: 1, field: 'Luonnontieteet', checked: false},
+    {fieldId: 2, field: 'Tekniikka', checked: false},
+    {fieldId: 3, field: 'Lääke- ja yritystieteet', checked: false},
+    {fieldId: 4, field: 'Maatalous- ja metsätieteet', checked: false},
+    {fieldId: 5, field: 'Yhteiskuntatieteet', checked: false},
+    {fieldId: 6, field: 'Humanistiset tieteet', checked: false},
+    {fieldId: 9, field: 'Muut tieteet', checked: false}
+  ];
+
+  combined: any[];
 
   constructor() { }
 
@@ -18,5 +29,24 @@ export class FilterMethodService {
       }
      });
     return merged.flat();
+  }
+
+  separateMinor(source) {
+    let mapped: any;
+    this.combined = [];
+    // console.log(source);
+    // Map fields by field & nested id
+    if (source && source.length > 0) {
+      mapped = source.map(majorField => ({ field: majorField.key, id: majorField.fieldId.buckets[0].key }));
+    }
+
+    // Loop through major fields & push all instances as separate arrays
+    for (let i = 1; i < mapped.length; i++) {
+      if (i === 7) { i = 9; }
+      if (mapped) {
+        this.combined.push(mapped.filter(obj => obj.id.toString().charAt(0).includes(i)));
+      }
+    }
+    return this.combined;
   }
 }
