4325cc96c32504b936f400b1dca071565eed44bb
Sauli Purhonen <sauli.purhonen@visma.com>
2019-09-18 16:27:59 +0300

    Removed check for active lang

--- src/app/component/home-page/home-page.component.ts
+++ src/app/component/home-page/home-page.component.ts
@@ -5,7 +5,7 @@
 //  :author: CSC - IT Center for Science Ltd., Espoo Finland servicedesk@csc.fi
 //  :license: MIT
 
-import { Component, OnInit, ViewChild, ElementRef, LOCALE_ID, Inject } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef} from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { SearchService } from '../../services/search.service';
 import { SortService } from '../../services/sort.service';
@@ -28,9 +28,7 @@ export class HomePageComponent implements OnInit {
   @ViewChild('srHeader') srHeader: ElementRef;
 
   constructor( private searchService: SearchService, private sortService: SortService, private searchBar: SearchBarComponent,
-               private titleService: Title, @Inject( LOCALE_ID ) protected localeId: string ) {
-                console.log('Locale:', localeId);
-  }
+               private titleService: Title ) { }
 
   public setTitle( newTitle: string) {
     this.titleService.setTitle( newTitle );
