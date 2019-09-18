decdabf006883242ea0f8bb4d2244ebdc1de639a
Péter Siket-Szász <peter.siketszasz@gmail.com>
2019-09-18 11:04:16 +0300

    Added temporary example, computed layout

--- src/app/component/visualisation/treemap/treemap.component.ts
+++ src/app/component/visualisation/treemap/treemap.component.ts
@@ -9,20 +9,56 @@ import * as d3 from 'd3';
 export class TreemapComponent implements OnInit, OnChanges {
   @Input() data;
 
-  tree;
+  treemap;
   hierarchy;
+  root;
+
+  svg;
+  grandparent;
+
+  margin = {top: 30, right: 30, bottom: 30, left: 30};
+  width = 900;
+  height = 600;
+  format = d3.format(',');
+
+  x = d3.scaleLinear()
+    .domain([0, this.width])
+    .range([0, this.width]);
+  y = d3.scaleLinear()
+    .domain([0, this.height])
+    .range([0, this.height]);
+
   constructor() { }
 
   ngOnInit() {
     this.hierarchy = ['publicationYear', 'publicationTypeCode'];
 
+    this.treemap = d3.treemap()
+      .size([this.width, this.height])
+      .paddingInner(0)
+      .round(false);
 
-    const treemap = d3.treemap();
+    this.svg = d3.select('svg')
+    .attr('width', this.width + this.margin.left + this.margin.right)
+    .attr('height', this.height + this.margin.bottom + this.margin.top)
+    .style('margin-left', -this.margin.left + 'px')
+    .style('margin-right', -this.margin.right + 'px')
+    .append('g')
+      .attr('transform', 'translate(' + this.margin.left + ',' + this.margin.top + ')')
+      .style('shape-rendering', 'crispEdges');
+
+    this.grandparent = this.svg.append('g').attr('class', 'grandparent');
+    this.grandparent.append('rect')
+      .attr('y', -this.margin.top)
+      .attr('width', this.width)
+      .attr('height', this.margin.top);
+    this.grandparent.append('text');
   }
 
   ngOnChanges(changes: SimpleChanges) {
     if (!changes.data.firstChange) {
-      console.log(this.createHierarchy(this.data.map(x => x._source), this.hierarchy));
+      this.root = this.createHierarchy(this.data.map(x => x._source), this.hierarchy);
+      this.treemap(this.root);
     }
   }
 
@@ -34,7 +70,11 @@ export class TreemapComponent implements OnInit, OnChanges {
 
     const tree = nest.entries(data);
     const root = d3.hierarchy({key: 'Data', values: tree}, d => d.values).count();
-    return d3.partition()(root);
+    return root;
+  }
+
+  display(data) {
+
   }
 
 }
