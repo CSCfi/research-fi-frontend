7d1ecfa5496841699d1825583e9ef5587875b03d
Péter Siket-Szász <peter.siketszasz@gmail.com>
2019-09-18 16:22:19 +0300

    Added styles and (not great) zoom fix

--- src/app/component/visualisation/treemap/treemap.component.ts
+++ src/app/component/visualisation/treemap/treemap.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
 import * as d3 from 'd3';
 import { HierarchyNode } from 'd3';
-import { HtmlAstPath } from '@angular/compiler';
 
 @Component({
   selector: 'app-treemap',
@@ -14,6 +13,8 @@ export class TreemapComponent implements OnInit, OnChanges {
   treemap: d3.TreemapLayout<any>;
   hierarchy;
   root: d3.HierarchyNode<any>;
+
+  remove;
 
   svg: d3.Selection<SVGElement, any, HTMLElement, any>;
   grandparent: d3.Selection<SVGElement, any, HTMLElement, any>;
@@ -59,14 +60,17 @@ export class TreemapComponent implements OnInit, OnChanges {
       .attr('y', -this.margin.top)
       .attr('width', this.width)
       .attr('height', this.margin.top);
-    this.grandparent.append('text');
+    this.grandparent.append('text')
+      .attr('x', 6)
+      .attr('y', 6 - this.margin.top)
+      .attr('dy', '.75em');
   }
 
   ngOnChanges(changes: SimpleChanges) {
     if (!changes.data.firstChange) {
       this.root = this.createHierarchy(this.data.map(x => x._source), this.hierarchy);
       console.log(this.root);
-      this.treemap(this.root);
+      this.treemap(this.root.sort((a, b) => b.value - a.value));
       this.display(this.root);
     }
   }
@@ -82,7 +86,12 @@ export class TreemapComponent implements OnInit, OnChanges {
     return root;
   }
 
+  filterChildren(d: d3.HierarchyNode<any>) {
+    return d.height > 1 ? d.children : [];
+  }
+
   display(d: d3.HierarchyNode<number>) {
+
     this.grandparent.datum(d.parent)
       .on('click', this.transition.bind(this))
       .select('text')
@@ -90,22 +99,22 @@ export class TreemapComponent implements OnInit, OnChanges {
     // Color
     this.grandparent.datum(d.parent)
       .select('rect')
-      .attr('fill', dd => '#ff0000');
+      .attr('fill', dd => '#f05010');
 
     this.g1 = this.svg.insert('g', '.grandparent')
       .datum(d)
       .attr('class', 'depth');
 
     this.g = this.g1.selectAll('g')
-      .data(d.children.filter(dd => dd.children))
+      .data(dd => this.filterChildren(dd))
       .enter()
       .append('g');
     // Add click handler and class to all gs with children
-    this.g.filter(dd => dd.height > 1)  // && true to fix typescript error
+    this.g.filter(dd => dd.height > 1)
           .classed('children', true)
-          .on('click', this.transition.bind(this));
+          .on('click', this.transition.bind(this))
     this.g.selectAll('.child')
-          .data(dd => dd.children || [dd])
+          .data(dd => this.filterChildren(dd))
           .enter().append('rect')
           .attr('class', 'child')
           .call(this.rect.bind(this));
@@ -132,6 +141,7 @@ export class TreemapComponent implements OnInit, OnChanges {
   }
 
   transition(d) {
+    console.log(d);
     if (this.transitioning || !d) { return; }
     this.transitioning = true;
     const g2 = this.display(d);
@@ -159,20 +169,19 @@ export class TreemapComponent implements OnInit, OnChanges {
     t1.selectAll('.foreignObj').call(this.foreign.bind(this));
     // Remove old node
     t1.on('end.remove', function() {
-      console.log(this.parentElement.children);
-      this.parentElement.children[0].remove();
+      this.parentElement.children[1 - d.depth].remove();
     });
     this.transitioning = false;
   }
 
   text(text) {
-    text.attr('x', d => this.x(d.x) + 6)
-        .attr('y', d => this.y(d.y) + 6);
+    text.attr('x', d => this.x(d.x))
+        .attr('y', d => this.y(d.y));
   }
 
   rect(rect) {
-    rect.attr('x', d => this.x(d.x0) + 6)
-        .attr('y', d => this.y(d.y0) + 6)
+    rect.attr('x', d => this.x(d.x0))
+        .attr('y', d => this.y(d.y0))
         .attr('width', d => this.x(d.x1) - this.x(d.x0))
         .attr('height', d => this.y(d.y1) - this.y(d.y0))
         .attr('fill', d => '#dddddd');
