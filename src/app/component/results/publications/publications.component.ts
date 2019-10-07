f98441155aad25b0c09ffa2a521e77ca27ed6003
Sauli Purhonen <sauli.purhonen@visma.com>
2019-10-07 12:53:05 +0300

    Reset sort after descending order

--- src/app/component/results/publications/publications.component.ts
+++ src/app/component/results/publications/publications.component.ts
@@ -34,17 +34,15 @@ export class PublicationsComponent implements OnInit {
   sortBy(sortBy) {
     const activeSort = this.route.snapshot.queryParams.sort || '';
     const [sortColumn, sortDirection] = this.sortService.sortBy(sortBy, activeSort);
-    let currentSort = sortColumn + (sortDirection ? 'Desc' : '');
+    let newSort = sortColumn + (sortDirection ? 'Desc' : '');
     // Reset sort
-    if (activeSort.slice(-4) === 'Desc') {
-      currentSort = '';
-    }
+    if (activeSort.slice(-4) === 'Desc') { newSort = ''; }
 
 
     this.router.navigate([],
       {
         relativeTo: this.route,
-        queryParams: { sort: currentSort },
+        queryParams: { sort: newSort },
         queryParamsHandling: 'merge'
       }
     );
