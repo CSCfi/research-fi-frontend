b6ffd6dbd7ceee00c09d204625d38c90c95ebab7
Sauli Purhonen <sauli.purhonen@visma.com>
2019-10-08 16:51:53 +0300

    Removed parentheses and asterisk from highlight

--- src/app/pipes/highlight.pipe.ts
+++ src/app/pipes/highlight.pipe.ts
@@ -11,15 +11,16 @@ export class HighlightSearch implements PipeTransform {
     if (!args) {
         return value;
     }
+    // Parantheses are removed because of regexp. Asterisk doesn't work with titles containing mulptiple different integers
+    const stripped = args.replace(/[\])}[{(]/g, '').replace(/\*/g, '');
     // 'gi' stands for case insensitive, use 'g' if needed for case sensitive
-    const source = new RegExp(args, 'gi');
+    const source = new RegExp(stripped, 'gi');
     const match = value.match(source);
 
     if (!match) {
         return value;
     }
-
-    const replacedValue = value.replace(source, '<mark>' + match[0] + '</mark>');
+    const replacedValue = value.replace(source, ('<mark>' + match[0] + '</mark>'));
     // Needs to be bypassed because of dynamic value
     return this.sanitizer.bypassSecurityTrustHtml(replacedValue);
     }
