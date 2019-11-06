041a249f9b4d9f03df32a851c9dda60c8d3ebf77
Sauli Purhonen <sauli.purhonen@visma.com>
2019-11-06 09:54:41 +0200

    Suggest component functionality and markup

---
+++ src/app/component/results/suggest/suggest.component.ts
@@ -0,0 +1,54 @@
+//  This file is part of the research.fi API service
+//
+//  Copyright 2019 Ministry of Education and Culture, Finland
+//
+//  :author: CSC - IT Center for Science Ltd., Espoo Finland servicedesk@csc.fi
+//  :license: MIT
+
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { SearchService } from '../../../services/search.service';
+import { TabChangeService } from '../../../services/tab-change.service';
+import { map } from 'rxjs/operators';
+import { Router, ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-suggest',
+  templateUrl: './suggest.component.html',
+  styleUrls: ['./suggest.component.scss']
+})
+export class SuggestComponent implements OnInit, OnDestroy {
+  suggests: string;
+  responseData: any;
+  errorMessage: any;
+  currentTab: { data: string; labelFi: string; labelEn: string; link: string; icon: string; };
+  tabSub: any;
+
+  constructor( private searchService: SearchService, public router: Router, private route: ActivatedRoute,
+               private tabChangeService: TabChangeService ) {  }
+
+  ngOnInit() {
+    this.getResultData();
+    this.tabSub = this.tabChangeService.currentTab.subscribe(tab => this.currentTab = tab);
+  }
+
+  getResultData() {
+    // Get data
+    this.searchService.getData()
+    .pipe(map(responseData => [responseData]))
+    .subscribe(
+      responseData => {
+        this.responseData = responseData,
+        this.suggests = this.responseData[0].suggest.mySuggestions[0];
+      }
+    );
+  }
+
+  navigate(term) {
+    this.searchService.singleInput = term;
+    this.router.navigate(['results/', this.currentTab.data, term])
+  }
+
+  ngOnDestroy() {
+    this.tabSub.unsubscribe();
+  }
+}
