abd7d0771a2d5249c48dffff637ed7597e841640
Sauli Purhonen <sauli.purhonen@visma.com>
2019-11-27 12:02:26 +0200

    Refactored highlight logic. Match all search terms

--- src/app/pipes/highlight.pipe.ts
+++ src/app/pipes/highlight.pipe.ts
@@ -8,20 +8,35 @@ export class HighlightSearch implements PipeTransform {
     constructor( private sanitizer: DomSanitizer ) {}
 
     transform(value: any, args: any): any {
-    if (!args) {
-        return value;
-    }
-    // Parantheses are removed because of regexp. Asterisk doesn't work with titles containing mulptiple different integers
-    const stripped = args.replace(/[\])}[{(]/g, '').replace(/\*/g, '');
-    // 'gi' stands for case insensitive, use 'g' if needed for case sensitive
-    const source = new RegExp(stripped, 'gi');
-    const match = value.match(source);
+        // Parantheses are removed because of regexp. Asterisk doesn't work with titles containing mulptiple different integers
+        args = args.replace(/[\])}[{(]/g, '').replace(/\*/g, '');
 
-    if (!match) {
-        return value;
-    }
-    const replacedValue = value.replace(source, ('<mark>' + match[0] + '</mark>'));
-    // Needs to be bypassed because of dynamic value
-    return this.sanitizer.bypassSecurityTrustHtml(replacedValue);
+        const valueArr = value.split(' ');
+        // Remove empty strings
+        const argsArr = args.split(' ').filter(Boolean);
+
+        // Return value if no input
+        if (!args) {
+            return value;
+        }
+
+        // Map value keys and loop through args, replace with tags
+        const match = valueArr.map((e) => {
+            argsArr.forEach(x => {
+                if (e.toLowerCase().includes(x.toLowerCase())) {
+                    // 'gi' stands for case insensitive, use 'g' if needed for case sensitive
+                    const src = new RegExp(x, 'gi');
+                    const found = e.match(src);
+                    if (!found) {return e; }
+                    e = e.replace(src, ('<mark>' + found[0] + '</mark>'));
+                 }
+            });
+            return e;
+        });
+
+        const result = match.join(' ');
+
+        // Needs to be bypassed because of dynamic value
+        return this.sanitizer.bypassSecurityTrustHtml(result);
     }
 }
