85c95c675206af703adc8a1a180c70554252d2d6
Sauli Purhonen <sauli.purhonen@visma.com>
2019-12-11 09:54:09 +0200

    Removed unnecessary array

--- src/app/component/single-publication/single-publication.component.ts
+++ src/app/component/single-publication/single-publication.component.ts
@@ -57,8 +57,6 @@ export class SinglePublicationComponent implements OnInit, OnDestroy {
     {label: 'Julkaisu\u00ADfoorumitaso', field: 'jufoClassCode', link: true, linkPath: '/results/publications?page=1&juFo='},
   ];
 
-  mediumTopRowFields = [];
-
   linksFields = [
     {label: 'DOI', field: 'doi', path: 'https://doi.org/'},
     {label: 'Pysyvä osoite', field: 'doiHandle', path: ''},
