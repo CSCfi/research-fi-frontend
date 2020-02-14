40f64ab8458bfd55a3bb783521ddaed19cff6885
Sauli Purhonen <sauli.purhonen@visma.com>
2020-02-14 15:21:54 +0200

    Shape data through publicationFilters class

--- src/app/component/results/filters/filters/filters.component.ts
+++ src/app/component/results/filters/filters/filters.component.ts
@@ -19,6 +19,8 @@ import { WINDOW } from 'src/app/services/window.service';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap';
 import { UtilityService } from 'src/app/services/utility.service';
 
+import { PublicationFilters } from './publications';
+
 @Component({
   selector: 'app-filters',
   templateUrl: './filters.component.html',
@@ -72,7 +74,7 @@ export class FiltersComponent implements OnInit, OnDestroy, OnChanges, AfterCont
                private filterService: FilterService, private resizeService: ResizeService,
                @Inject(WINDOW) private window: Window, private modalService: BsModalService,
                private route: ActivatedRoute, private utilityService: UtilityService,
-               private sortService: SortService ) {
+               private sortService: SortService, private publicationFilters: PublicationFilters ) {
                 this.height = 220;
                 this.clickCount = 0;
                 this.selectedFilters = [];
@@ -156,7 +158,21 @@ export class FiltersComponent implements OnInit, OnDestroy, OnChanges, AfterCont
       }
     }
 
-    this.shapeData();
+    this.shapeData(this.tabData);
+  }
+
+  // Shape response data by index
+  shapeData(tab) {
+    this.responseData = this.responseData || [];
+
+    if (this.responseData.length > 0) {
+      switch (tab) {
+        case 'publications': {
+          this.publicationFilters.shapeData(this.responseData);
+        }
+      }
+    }
+    this.cdr.detectChanges();
   }
 
   // Navigate
@@ -248,185 +264,6 @@ export class FiltersComponent implements OnInit, OnDestroy, OnChanges, AfterCont
     this.clickCount++;
     total = total - 5 * this.clickCount;
     this.height = total < 5 ? this.height + total * 48 : this.height = this.height * 2;
-  }
-
-  shapeData() {
-    this.responseData = this.responseData || [];
-
-    if (this.responseData.length > 0) {
-      const source = this.responseData[0].aggregations;
-      // Organization & sector
-      this.organization(source.organization);
-      // Major field
-      source.field.buckets = this.minorField(source.field.buckets);
-      // Publication Type
-      source.publicationType.buckets = this.separatePublicationClass(source.publicationType.buckets);
-      // Country code
-      source.countryCode.buckets = this.publicationCountry(source.countryCode.buckets);
-      // Jufo code
-      source.juFo.buckets = this.juFoCode(source.juFo.buckets);
-      // Open access
-      source.openAccess.buckets = this.openAccess(source.openAccess.buckets, source.selfArchived.buckets);
-      // Internationatl collaboration
-      source.internationalCollaboration.buckets = this.getSingleAmount(source.internationalCollaboration.buckets);
-      // console.log(source);
-    }
-    this.cdr.detectChanges();
-  }
-
-
-  organization(data) {
-    const org = data.sectorName;
-    data.buckets = org.buckets;
-    data.buckets.forEach(item => {
-      item.subData = item.organizations.buckets;
-      item.subData.map(subItem => {
-        subItem.label = subItem.key;
-        subItem.key = subItem.orgId.buckets[0].key;
-      });
-    });
-  }
-
-  minorField(data) {
-      // check if major aggregation is available
-      const combinedMajorFields =  data ?
-      (this.filterMethodService.separateMinor(data ? data : []) ) : [];
-
-      const result = this.staticDataService.majorFieldsOfScience;
-      for (let i = 0; i < combinedMajorFields.length; i++) {
-        if (result[i]) {
-          result[i].subData = combinedMajorFields[i];
-        }
-      }
-      return result;
-  }
-
-  separatePublicationClass(data) {
-    const source = this.responseData[0] ? this.responseData[0].aggregations.publicationType.buckets : [];
-    let combined = [];
-    if (source && source.length > 0) {
-      source.forEach(val => combined.push(val.key.substring(0, 1)));
-      combined.filter((v, i, a) => a.indexOf(v) === i);
-    }
-    combined = [...new Set(combined)];
-    const staticData = this.staticDataService.publicationClass;
-
-    // Map items for subData
-    const result = combined.map(
-      x => x = {key: x + ', ' + staticData.find(item => item.class === x).label, subData: staticData.find(item => item.class === x)
-        .types.map(type => type = {
-          type: type.type,
-          label: type.type + ', ' + type.label,
-          key: type.type,
-          doc_count: data.find(doc => doc.key === type.type) ? data.find(doc => doc.key === type.type).doc_count : ''
-        })}
-      );
-
-    return result;
-  }
-
-  publicationCountry(data) {
-    const result = data.map(item =>
-      item = {key: 'c' + item.key, label: item.key === 0 ? 'Suomi' : 'Muu', doc_count: item.doc_count, value: item.key});
-    return result;
-  }
-
-  juFoCode(data) {
-    const staticData = this.staticDataService.juFoCode;
-    const result = data.map(item => item = {
-      key: item.key === ' ' ? 'noVal' : 'j' + item.key,
-      label: staticData.find(code => code.key === item.key).labelFi,
-      doc_count: item.doc_count,
-      value: item.key
-    });
-    return result;
-  }
-
-  openAccess(openAccess, selfArchived) {
-    let openAccessCodes = [];
-    const result = [];
-    // Get aggregation from response
-    if (openAccess && openAccess.length > 0) {
-      openAccess.forEach(val => {
-        switch (val.key) {
-          case 1: {
-            openAccessCodes.push({key: 'openAccess', doc_count: val.doc_count, label: 'Open Access -lehti'});
-            break;
-          }
-          case 2: {
-            openAccessCodes.push({key: 'otherOpen', doc_count: val.doc_count, label: 'Muu avoin saatavuus'});
-            break;
-          }
-          case 0: {
-            openAccessCodes.push({key: 'nonOpenAccess', doc_count: val.doc_count, label: 'Ei avoin'});
-            break;
-          }
-          default: {
-            openAccessCodes.push({key: 'noOpenAccessData', doc_count: val.doc_count, label: 'Ei tietoa'});
-            break;
-          }
-        }
-      });
-    }
-    if (selfArchived && selfArchived.length > 0) {
-      selfArchived.forEach(val => {
-        switch (val.key) {
-          case 1: {
-            openAccessCodes.push({key: 'selfArchived', doc_count: val.doc_count, label: 'Rinnakkaistallennettu'});
-            break;
-          }
-          case 0: {
-            openAccessCodes.push({key: 'selfArchivedNonOpen', doc_count: val.doc_count, label: 'Ei avoin'});
-            break;
-          }
-          default: {
-            openAccessCodes.push({key: 'noOpenAccessData', doc_count: val.doc_count, label: 'Ei tietoa'});
-            break;
-          }
-        }
-      });
-    }
-
-    // Get duplicate values and sum doc counts
-    const reduce = openAccessCodes.reduce((item, val) => {
-      const sum = item.filter((obj) => {
-          return obj.key === val.key;
-      }).pop() || {key: val.key, doc_count: 0, label: val.label};
-
-      sum.doc_count += val.doc_count;
-      item.push(sum);
-      return item;
-    }, []);
-
-    // Remove duplicates
-    openAccessCodes = [...new Set(reduce)];
-
-    function docCount(key) {return openAccessCodes.find(item => item.key === key).doc_count; }
-
-    // Push items by key
-    if (openAccessCodes.some(e => e.key === 'openAccess')) {
-      result.push({key: 'openAccess', doc_count: docCount('openAccess'), label: 'Open Access -lehti'});
-    }
-    if (openAccessCodes.some(e => e.key === 'otherOpen')) {
-      result.push({key: 'otherOpen', doc_count: docCount('otherOpen'), label: 'Rinnakkaistallennettu'});
-    }
-    if (openAccessCodes.some(e => e.key === 'selfArchived')) {
-      result.push({key: 'selfArchived', doc_count: docCount('selfArchived'), label: 'Muu avoin saatavuus'});
-    }
-    if (openAccessCodes.some(e => e.key === 'nonOpenAccess') && openAccessCodes.some(e => e.key === 'selfArchivedNonOpen')) {
-      result.push({key: 'nonOpen', doc_count: Math.max(docCount('nonOpenAccess'), docCount('selfArchivedNonOpen')),  label: 'Ei avoin'});
-    }
-    if (openAccessCodes.some(e => e.key === 'noOpenAccessData')) {
-      result.push({key: 'noOpenAccessData', doc_count: docCount('noOpenAccessData'), label: 'Ei tietoa'});
-    }
-
-    return result;
-  }
-
-  getSingleAmount(data) {
-    if (data.length > 0) {
-      return data.filter(x => x.key === 1);
-    }
   }
 
 }
