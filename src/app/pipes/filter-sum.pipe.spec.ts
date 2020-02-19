a063d0b1e8efaa8a58288f3cde02ba1e2bbebc92
Sauli Purhonen <sauli.purhonen@visma.com>
2020-02-19 11:18:38 +0200

    Removed spec file

--- src/app/pipes/filter-sum.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FilterSumPipe } from './filter-sum.pipe';
-
-describe('FilterSumPipe', () => {
-  it('create an instance', () => {
-    const pipe = new FilterSumPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
