7311486b5b46e09234bc3da0d3a7f94dc293b2e0
Sauli Purhonen <sauli.purhonen@visma.com>
2020-03-05 14:19:51 +0200

    All content as objects in allContent var, onSectionChange method

--- src/app/component/science-politics/figures/figures.component.ts
+++ src/app/component/science-politics/figures/figures.component.ts
@@ -20,10 +20,10 @@ export class FiguresComponent implements OnInit {
   faChartBar = faChartBar;
 
   navItems = [
-    {labelFi: 'Tiede ja tutkimus lukuina', icon: this.faIconCircle, active: true},
-    {labelFi: 'Tutkimuksen henkilöstövoimavarat ja rahoitus', icon: this.faChartBar, active: false},
-    {labelFi: 'Julkaisutoiminta ja tieteellinen vaikuttavuus', icon: this.faChartBar, active: false},
-    {labelFi: 'Tutkimus- ja kehittämistoiminnan intensiteetti', icon: this.faChartBar, active: false},
+    {id: 1, labelFi: 'Tiede ja tutkimus lukuina', icon: this.faIconCircle, active: true},
+    {id: 2, labelFi: 'Tutkimuksen henkilöstövoimavarat ja rahoitus', icon: this.faChartBar, active: false},
+    {id: 3, labelFi: 'Julkaisutoiminta ja tieteellinen vaikuttavuus', icon: this.faChartBar, active: false},
+    {id: 4, labelFi: 'Tutkimus- ja kehittämistoiminnan intensiteetti', icon: this.faChartBar, active: false},
   ];
 
   coLink = [
@@ -32,16 +32,38 @@ export class FiguresComponent implements OnInit {
     {labelFi: 'Vipunen'},
   ];
 
-  content1 = [
-    {labelFi: 'Yliopistojen opetus ja tutkimushenkilöstön henkilötyövuodet uraportaittain', descriptionFi: 'Kuvaus', img: ''},
-    {labelFi: 'Yliopistojen kokonaisrahoitus, valtionrahoitus ja tutkimuksen rahoituslähteet', descriptionFi: 'Kuvaus', img: ''},
-    {labelFi: 'Ulkomaalaisen opetus- ja tutkimus-henkilöstön henkilötyövuodet sekä osuus kaikista henkilötyövuosista yliopistoittain ja uraportaittain', descriptionFi: 'Kuvaus', img: ''},
-    {labelFi: 'Tutkimustyövuosien indeksoitu kehitys yliopistoissa ja valtion tutkimuslaitoksissa', descriptionFi: 'Kuvaus', img: ''},
+  allContent = [
+    {id: 2, headerFi: 'Tutkimuksen henkilöstövoimavarat ja rahoitus', items: [
+      {labelFi: 'Yliopistojen opetus ja tutkimushenkilöstön henkilötyövuodet uraportaittain', descriptionFi: 'Kuvaus', img: 'assets/figure.jpg'},
+      {labelFi: 'Yliopistojen kokonaisrahoitus, valtionrahoitus ja tutkimuksen rahoituslähteet', descriptionFi: 'Kuvaus', img: 'assets/figure.jpg'},
+      {labelFi: 'Ulkomaalaisen opetus- ja tutkimus-henkilöstön henkilötyövuodet sekä osuus kaikista henkilötyövuosista yliopistoittain ja uraportaittain', descriptionFi: 'Kuvaus', img: 'assets/figure.jpg'},
+      {labelFi: 'Tutkimustyövuosien indeksoitu kehitys yliopistoissa ja valtion tutkimuslaitoksissa', descriptionFi: 'Kuvaus', img: 'assets/figure.jpg'},
+    ]},
+    {id: 3, headerFi: 'Julkaisutoiminta ja tieteellinen vaikuttavuus', items: [
+      {labelFi: 'OECD-maiden ja Kiinan tieteellisen vaikuttavuuden kehitys Top 10 -indeksillä tarkasteltuna', descriptionFi: 'Kuvaus', img: 'assets/figure.jpg'},
+      {labelFi: 'Suomen julkaisumäärän suhteellinen muutos', descriptionFi: 'Kuvaus', img: 'assets/figure.jpg'},
+      {labelFi: 'Suomen julkaisujen tieteellinen vaikuttavuus julkaisuyhteistyön mukaan', descriptionFi: 'Kuvaus', img: 'assets/figure.jpg'},
+      {labelFi: 'Suomen yhteistyömaat', descriptionFi: 'Kuvaus', img: 'assets/figure.jpg'},
+      {labelFi: 'Yliopiston osuus tieteenalaryhmän julkaisuista yliopistoissa', descriptionFi: 'Kuvaus', img: 'assets/figure.jpg'},
+    ]},
+    {id: 4, headerFi: 'Tutkimus- ja kehittämistoiminnan intensiteetti', items: [
+      {labelFi: 'Tohtoreiden tutkimustyövuodet sektoreittain', descriptionFi: 'Kuvaus', img: 'assets/figure.jpg'},
+      {labelFi: 'Suomen t&k menot ja t&k intensiteetti suorittajasektoreittain', descriptionFi: 'Kuvaus', img: 'assets/figure.jpg'},
+      {labelFi: 'Suomen t&k intensiteetti kansainvälisessä vertailussa', descriptionFi: 'Kuvaus', img: 'assets/figure.jpg'},
+    ]}
   ];
 
-  constructor() { }
+  description = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+  currentSection: any;
+
+  constructor() {
+    this.currentSection = 1;
+   }
 
   ngOnInit(): void {
   }
 
+  onSectionChange(sectionId: any) {
+    this.currentSection = parseInt(sectionId, 10);
+  }
 }
