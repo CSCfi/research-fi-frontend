299696b1c3d3641c324461f12e51c4104e809eb2
Sauli Purhonen <sauli.purhonen@visma.com>
2020-03-05 14:46:27 +0200

    For of loop

--- src/app/directives/scroll-spy.directive.ts
+++ src/app/directives/scroll-spy.directive.ts
@@ -5,26 +5,25 @@ import { Directive, Injectable, Input, EventEmitter, Output, ElementRef, HostLis
 })
 export class ScrollSpyDirective {
   @Input() public spiedTags = [];
-  @Output() public sectionChange = new EventEmitter<number>();
-  private currentSection: number;
+  @Output() public sectionChange = new EventEmitter<string>();
+  private currentSection: string;
 
   constructor(private el: ElementRef) {}
 
   @HostListener('window:scroll', ['$event']) onWindowScroll(event: any) {
-    let currentSection: number;
+    let currentSection: string;
     const children = this.el.nativeElement.children;
     const scrollTop = event.target.firstElementChild.scrollTop;
     const parentOffset = event.target.firstElementChild.offsetTop;
-    // console.log(children);
-    for (let i = 0; i < children.length; i++) {
-        const element = children[i];
-        if (this.spiedTags.some(spiedTag => spiedTag === element.tagName)) {
-          if ((element.offsetTop - parentOffset) <= scrollTop) {
-            currentSection = element.id;
-          }
+    console.log(children);
+    for (const [i, v] of Object.keys(children)) {
+      const element = children[i];
+      if (this.spiedTags.some(spiedTag => spiedTag === element.tagName)) {
+        if ((element.offsetTop - parentOffset) <= scrollTop) {
+          currentSection = element.id;
         }
+      }
     }
-    // console.log(currentSection);
     if (currentSection !== this.currentSection) {
       this.currentSection = currentSection;
       this.sectionChange.emit(this.currentSection);
