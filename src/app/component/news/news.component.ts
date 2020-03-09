0ddc2845c2cec4f672896b8ca1db98c58331c10c
Sauli Purhonen <sauli.purhonen@visma.com>
2020-03-09 14:43:08 +0200

    Get news via getNews method

--- src/app/component/news/news.component.ts
+++ src/app/component/news/news.component.ts
@@ -1,4 +1,13 @@
+//  This file is part of the research.fi API service
+//
+//  Copyright 2019 Ministry of Education and Culture, Finland
+//
+//  :author: CSC - IT Center for Science Ltd., Espoo Finland servicedesk@csc.fi
+//  :license: MIT
+
 import { Component, OnInit } from '@angular/core';
+import { SearchService } from 'src/app/services/search.service';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-news',
@@ -6,10 +15,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./news.component.scss']
 })
 export class NewsComponent implements OnInit {
+  data: any;
+  errorMessage: any;
 
-  constructor() { }
+  constructor( private searchService: SearchService) { }
 
   ngOnInit() {
+    this.getNews();
+  }
+
+  getNews() {
+    this.searchService.getNews()
+    .pipe(map(data => [data]))
+    .subscribe(data => {
+      this.data = data;
+      console.log(this.data);
+    }, error => this.errorMessage = error as any);
   }
 
 }
