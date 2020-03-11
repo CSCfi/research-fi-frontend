43aa1259047095e0d701221ff0433a8fb3464854
Sauli Purhonen <sauli.purhonen@visma.com>
2020-03-11 13:54:34 +0200

    Related links component

---
+++ src/app/component/single/related-links/related-links.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit, Input, OnDestroy } from '@angular/core';
+import { SingleItemService } from '../../../services/single-item.service';
+import { ActivatedRoute, Router, UrlTree, UrlSegmentGroup, PRIMARY_OUTLET, UrlSegment } from '@angular/router';
+
+@Component({
+  selector: 'app-related-links',
+  templateUrl: './related-links.component.html',
+  styleUrls: ['./related-links.component.scss']
+})
+export class RelatedLinksComponent implements OnInit, OnDestroy {
+  @Input() id: any;
+
+  relatedList = [
+    {labelFi: 'Julkaisut', tab: 'publications', disabled: true},
+    {labelFi: 'Tutkijat', tab: 'persons', disabled: true},
+    {labelFi: 'Aineistot', tab: '', disabled: true},
+    {labelFi: 'Infrastruktuurit', tab: 'infrastructures', disabled: true},
+    {labelFi: 'Muu tutkimustoiminta', tab: '', disabled: true},
+    {labelFi: 'Organisaatiot', tab: 'organizations', disabled: true},
+  ];
+
+  docCountData: any;
+  currentParent: string;
+  routeSub: any;
+
+  constructor( private singleService: SingleItemService, private route: ActivatedRoute, private router: Router ) {
+    this.docCountData = [];
+   }
+
+  ngOnInit(): void {
+    // Subscribe to route params, get current parent from current url
+    this.routeSub = this.route.params.subscribe(param => {
+      const currentUrl = this.router.url;
+      const tree: UrlTree = this.router.parseUrl(currentUrl);
+      const g: UrlSegmentGroup = tree.root.children[PRIMARY_OUTLET];
+      const s: UrlSegment[] = g.segments;
+      this.currentParent = s[1].path + 's';
+    })
+    this.getDocCounts(this.id);
+  }
+
+  // Get doc counts with single service getCount method, assign to to docCountData and show in appropriate counts in template
+  getDocCounts(id: string) {
+    this.singleService.getCount(id).subscribe((data) => {
+      this.docCountData = data;
+      this.docCountData = this.docCountData.aggregations._index.buckets;
+      // Set related list item to disabled to false if matching item has docs
+      this.relatedList.map(item => item.disabled = this.docCountData[item.tab]?.doc_count > 0 ? false : true);
+    });
+  }
+
+  ngOnDestroy() {
+    this.routeSub.unsubscribe();
+  }
+
+}
