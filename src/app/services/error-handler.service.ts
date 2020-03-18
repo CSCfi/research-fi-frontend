a6186f3e1ddac265157e98246ea9f0fa54af8e01
Timo Särkikoski <sarkikos@gmail.com>
2020-03-18 10:48:47 +0200

    Added Elastic application performance monitoring service.

--- src/app/services/error-handler.service.ts
+++ src/app/services/error-handler.service.ts
@@ -1,11 +1,13 @@
 import { Injectable, ErrorHandler } from '@angular/core';
 import { LoggerService } from './logger.service';
+//import { ApmErrorHandler } from '@elastic/apm-rum-angular'
 
 @Injectable({
   providedIn: 'root'
 })
-export class ErrorHandlerService extends ErrorHandler {
 
+//export class ErrorHandlerService extends ApmErrorHandler {
+export class ErrorHandlerService extends ErrorHandler {
   constructor(private logger: LoggerService) {
     super();
   }
