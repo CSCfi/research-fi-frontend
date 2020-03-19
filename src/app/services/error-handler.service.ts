4f943e2ba436d3e72793d6ceda45eaf971a26498
Timo Särkikoski <sarkikos@gmail.com>
2020-03-19 07:39:14 +0200

    Added comments about Application Performance Monitoring

--- src/app/services/error-handler.service.ts
+++ src/app/services/error-handler.service.ts
@@ -6,7 +6,16 @@ import { LoggerService } from './logger.service';
   providedIn: 'root'
 })
 
-//export class ErrorHandlerService extends ApmErrorHandler {
+// APM - https://www.elastic.co/guide/en/apm/agent/rum-js/current/angular-integration.html
+//
+// ErrorHandlerService should extend AmpErrorHandler, which takes care of sending
+// error reports to the APM server. At the moment the APM JS client does not work
+// properly with build target "es2015" in tsconfig.json. It would work using target "es5".
+// ApmErrorHandler should be activated when the following issue is fixed:
+// https://github.com/elastic/apm-agent-rum-js/issues/607
+
+// export class ErrorHandlerService extends ApmErrorHandler {
+
 export class ErrorHandlerService extends ErrorHandler {
   constructor(private logger: LoggerService) {
     super();
