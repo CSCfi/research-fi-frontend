ab834510ca4bfed56ada690c81df44e7912cba78
saulipurhonen <sauli.purhonen@gmail.com>
2020-03-24 10:17:06 +0200

    OKM url based on locale

--- src/app/ui/footer/footer.component.ts
+++ src/app/ui/footer/footer.component.ts
@@ -5,7 +5,7 @@
 // # :author: CSC - IT Center for Science Ltd., Espoo Finland servicedesk@csc.fi
 // # :license: MIT
 
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, LOCALE_ID } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { AppConfigService } from '../../services/app-config-service.service';
 import { faTwitter, faFacebook, faLinkedin } from '@fortawesome/free-brands-svg-icons';
@@ -15,17 +15,37 @@ import { faTwitter, faFacebook, faLinkedin } from '@fortawesome/free-brands-svg-
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
+
 export class FooterComponent implements OnInit {
   buildInfo = '';
   faTwitter = faTwitter;
   faFacebook = faFacebook;
   faLinkedin = faLinkedin;
+  okmUrl: string;
 
-  constructor(private appConfigService: AppConfigService) {
+  constructor(private appConfigService: AppConfigService, @Inject(LOCALE_ID) protected localeId: string) {
     this.buildInfo = this.appConfigService.buildInfo;
   }
 
   ngOnInit() {
+    this.translateContent();
+  }
+
+  translateContent() {
+    switch (this.localeId) {
+      case 'fi': {
+        this.okmUrl = 'https://www.minedu.fi';
+        break;
+      }
+      case 'en': {
+        this.okmUrl = 'https://minedu.fi/en/';
+        break;
+      }
+      case 'sv': {
+        this.okmUrl = 'https://minedu.fi/sv/';
+        break;
+      }
+    }
   }
 
 }
