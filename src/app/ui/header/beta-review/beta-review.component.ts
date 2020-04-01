5bf6a15c5700678b0476c54ad0d895c66528116b
saulipurhonen <sauli.purhonen@gmail.com>
2020-04-01 11:28:21 +0300

    Beta review component as dialog

---
+++ src/app/ui/header/beta-review/beta-review.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-beta-review',
+  templateUrl: './beta-review.component.html',
+  styleUrls: ['./beta-review.component.scss']
+})
+export class BetaReviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
