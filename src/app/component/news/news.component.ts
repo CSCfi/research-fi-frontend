a73b8c0a50893930e0ad0a3c0de12e1be513bae5
saulipurhonen <sauli.purhonen@gmail.com>
2020-04-08 10:59:33 +0300

    Focus main content / input

--- src/app/component/news/news.component.ts
+++ src/app/component/news/news.component.ts
@@ -5,21 +5,26 @@
 //  :author: CSC - IT Center for Science Ltd., Espoo Finland servicedesk@csc.fi
 //  :license: MIT
 
-import { Component, OnInit, Inject, LOCALE_ID } from '@angular/core';
+import { Component, OnInit, Inject, LOCALE_ID, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import { SearchService } from 'src/app/services/search.service';
 import { map } from 'rxjs/operators';
 import { Title } from '@angular/platform-browser';
+import { TabChangeService } from 'src/app/services/tab-change.service';
 
 @Component({
   selector: 'app-news',
   templateUrl: './news.component.html',
   styleUrls: ['./news.component.scss']
 })
-export class NewsComponent implements OnInit {
+export class NewsComponent implements OnInit, AfterViewInit {
   data: any;
   errorMessage: any;
+  focusSub: any;
+  @ViewChild('searchInput') searchInput: ElementRef;
+  @ViewChild('mainFocus') mainFocus: ElementRef;
 
-  constructor( private searchService: SearchService, private titleService: Title, @Inject(LOCALE_ID) protected localeId: string) { }
+  constructor( private searchService: SearchService, private titleService: Title, @Inject(LOCALE_ID) protected localeId: string,
+               private tabChangeService: TabChangeService) { }
 
   ngOnInit() {
     this.getNews();
@@ -36,6 +41,19 @@ export class NewsComponent implements OnInit {
       }
     }
   }
+
+  ngAfterViewInit() {
+    // Focus with skip-links
+    this.focusSub = this.tabChangeService.currentFocusTarget.subscribe(target => {
+      if (target === 'search-input') {
+        this.searchInput.nativeElement.focus();
+      }
+      if (target === 'main-link') {
+        this.mainFocus.nativeElement.focus();
+      }
+    });
+  }
+
 
   public setTitle( newTitle: string) {
     this.titleService.setTitle( newTitle );
