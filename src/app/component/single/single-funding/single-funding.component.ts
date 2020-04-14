1421fbbb21dead28132bd6094d32f5a10786450b
saulipurhonen <sauli.purhonen@gmail.com>
2020-04-14 11:35:27 +0300

    Changes to view

--- src/app/component/single/single-funding/single-funding.component.ts
+++ src/app/component/single/single-funding/single-funding.component.ts
@@ -68,11 +68,12 @@ export class SingleFundingComponent implements OnInit, OnDestroy {
   ];
 
   otherFields = [
-    {label: 'Tieteenala', field: 'fieldsOfScience'},
+    {label: 'Rahoituspäätöksen numero', field: 'funderProjectNumber'},
+    {label: 'Tieteenalat', field: 'fieldsOfScience'},
     {label: 'Tutkimusalat', field: 'fieldsOfResearch'},
-    {label: 'Teema-ala', field: '?'},
+    {label: 'Teema-alat', field: '?'},
+    {label: 'Hankkeen verkkosivu', field: '?'},
     // {label: 'Avainsanat', field: 'keywords'},
-    {label: 'Rahoituspäätöksen numero', field: 'funderProjectNumber'}
   ];
 
   linkFields = [
@@ -129,6 +130,7 @@ export class SingleFundingComponent implements OnInit, OnDestroy {
         this.srHeader.nativeElement.innerHTML = this.titleService.getTitle().split(' - ', 1);
         this.shapeData();
         this.filterData();
+        console.log(this.responseData.fundings[0]);
       }
     },
       error => this.errorMessage = error as any);
