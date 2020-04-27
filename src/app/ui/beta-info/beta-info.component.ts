4eea918aaa42702b9d6fe837e4bdeecdc670f697
saulipurhonen <sauli.purhonen@gmail.com>
2020-04-27 14:31:28 +0300

    Beta info component

---
+++ src/app/ui/beta-info/beta-info.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-beta-info',
+  templateUrl: './beta-info.component.html',
+  styleUrls: ['./beta-info.component.scss']
+})
+export class BetaInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
