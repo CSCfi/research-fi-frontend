f88a7ca78e1227ef35ce5b2965fc9b6aa3e83717
saulipurhonen <sauli.purhonen@gmail.com>
2020-05-07 12:03:46 +0300

    Spread operator on constructFilters method in constructQuery

--- src/app/services/filter.service.ts
+++ src/app/services/filter.service.ts
@@ -293,7 +293,7 @@ export class FilterService {
           must: [
             { term: { _index: index } },
             ...(searchTerm ? [query] : []),
-            this.constructFilters(index)
+            ...this.constructFilters(index)
           ],
         }
     };
