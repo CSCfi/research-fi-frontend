0a8e01d74ac893ecb4381c61e236f69484c114d4
saulipurhonen <sauli.purhonen@gmail.com>
2020-05-07 09:36:04 +0300

    Separate translation for organization filter on publications and fundings

--- src/app/component/results/active-filters/active-filters.component.ts
+++ src/app/component/results/active-filters/active-filters.component.ts
@@ -206,8 +206,21 @@ export class ActiveFiltersComponent implements OnInit, OnDestroy, AfterContentIn
               }
             }
 
-            // Organization name
-            if (val.category === 'organization' && source.organization) {
+            // Publication organization name
+            if (tab === 'publications' && val.category === 'organization' && source.organization) {
+              if (source.organization.sectorName && source.organization.sectorName.buckets.length > 0) {
+                source.organization.sectorName.buckets.forEach(sector => {
+                  sector.organization.buckets.forEach(org => {
+                    if (org.orgId.buckets[0].key === val.value) {
+                      const foundIndex = this.activeFilters.findIndex(x => x.value === val.value);
+                      this.activeFilters[foundIndex].translation = org.key.trim();
+                    }
+                  });
+                });
+              }
+            }
+            // Funding organization name
+            if (this.currentTab === 'fundings' && val.category === 'organization' && source.organization) {
               if (source.organization.sectorName && source.organization.sectorName.buckets.length > 0) {
                 source.organization.sectorName.buckets.forEach(sector => {
                   sector.organizations.buckets.forEach(org => {
@@ -219,6 +232,7 @@ export class ActiveFiltersComponent implements OnInit, OnDestroy, AfterContentIn
                 });
               }
             }
+
             // Country code
             if (val.category === 'countryCode' && source.countryCode) {
               switch (val.value) {
