4f92531c8bde85b8159dc0a0ce14841057a6e725
Péter Siket-Szász <peter.siket-szasz@csc.fi>
2020-05-13 14:47:41 +0300

    Fixed scroll and arrow navigation on result tab

--- src/app/component/result-tab/tab-item/tab-item.component.ts
+++ src/app/component/result-tab/tab-item/tab-item.component.ts
@@ -1,12 +1,13 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, QueryList, AfterViewInit, ElementRef } from '@angular/core';
 import { SearchService } from 'src/app/services/search.service';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-tab-item',
   templateUrl: './tab-item.component.html',
   styleUrls: ['./tab-item.component.scss']
 })
-export class TabItemComponent implements OnInit {
+export class TabItemComponent implements OnInit, AfterViewInit {
   @Input() tab: any;
   @Input() isHomepage = false;
   @Input() selectedTab: string;
@@ -14,9 +15,14 @@ export class TabItemComponent implements OnInit {
   @Input() counted: any;
   @Input() locale: string;
 
-  constructor(public searchService: SearchService) { }
+  @ViewChild('tabList') tabElem: ElementRef;
+
+  constructor(public searchService: SearchService, private dataService: DataService) { }
 
   ngOnInit(): void {
   }
 
+  ngAfterViewInit(): void {
+    if (this.tabElem) this.dataService.resultTabList.push(this.tabElem);
+  }
 }
