8767ce4d14c442200287912c3f25fa7bbde86b7d
saulipurhonen <sauli.purhonen@gmail.com>
2020-05-20 08:44:02 +0300

    Moved privacy obsevables into privacy service, updated paths

---
+++ src/app/services/privacy.service.ts
@@ -0,0 +1,29 @@
+//  This file is part of the research.fi API service
+//
+//  Copyright 2019 Ministry of Education and Culture, Finland
+//
+//  :author: CSC - IT Center for Science Ltd., Espoo Finland servicedesk@csc.fi
+//  :license: MIT
+
+import { Injectable } from '@angular/core';
+import { Subscription, BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PrivacyService {
+  private consentBarSource = new BehaviorSubject(false);
+  currentConsentBarStatus = this.consentBarSource.asObservable();
+  private consentStatusSource = new BehaviorSubject('declined');
+  currentConsentStatus = this.consentStatusSource.asObservable();
+
+  constructor() { }
+
+  hideConsentBar(status) {
+    this.consentBarSource.next(status);
+  }
+
+  changeConsentStatus(status) {
+    this.consentStatusSource.next(status);
+  }
+}
