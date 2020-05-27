fcbe103647facbad74a54cb5e9397b84ead02640
saulipurhonen <sauli.purhonen@gmail.com>
2020-05-27 10:57:22 +0300

    COmment

--- src/app/component/results/active-filters/active-filters.component.ts
+++ src/app/component/results/active-filters/active-filters.component.ts
@@ -146,6 +146,7 @@ export class ActiveFiltersComponent implements OnInit, OnDestroy, AfterContentIn
       });
       // Subscribe to aggregation data
       this.filterResponse = this.dataService.currentResponse.subscribe(response => {
+        // console.log(response);
         this.response = response;
         if (response) {
           const source = this.response.aggregations;
