3dae6fa5811c2224eacedc28d0ab4fce9d89102c
Timo Särkikoski <sarkikos@gmail.com>
2020-06-17 08:41:05 +0300

    Modified SearchService unit test

--- src/app/services/search.service.spec.ts
+++ src/app/services/search.service.spec.ts
@@ -1,3 +1,10 @@
+//  This file is part of the research.fi API service
+//
+//  Copyright 2019 Ministry of Education and Culture, Finland
+//
+//  :author: CSC - IT Center for Science Ltd., Espoo Finland servicedesk@csc.fi
+//  :license: MIT
+
 import { TestBed } from '@angular/core/testing';
 import { FilterService } from './filter.service';
 import { SortService } from './sort.service';
@@ -6,7 +13,9 @@ import { SearchService } from './search.service';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing'
 import { AppConfigService } from './app-config-service.service';
 
-import ResponseJson from '../testdata/searchresponse.json';
+import ResponseJsonPublications from '../../testdata/searchresponse-publications.json';
+import ResponseJsonFundings from '../../testdata/searchresponse-fundings.json';
+import ResponseJsonInfrastructures from '../../testdata/searchresponse-infrastructures.json';
 
 const mockApiUrl = 'test.api.fi/'
 
@@ -41,14 +50,13 @@ describe('SearchService', () => {
     expect(searchService).toBeTruthy();
   });
 
-  it('should send API request to correct URL', () => {
-    const searchResponseMock = ResponseJson;
-    console.log(searchResponseMock);
+  it('should send API request to publication/_search from publications tab', () => {
+    const searchResponseMock = ResponseJsonPublications;
     const http = TestBed.get(HttpTestingController);
     const tabChangeService = TestBed.get(TabChangeService);
     let searchResponse;
 
-    searchService.singleInput = 'dadaa';
+    searchService.singleInput = 'searchtext';
     searchService.fromPage = 1;
     tabChangeService.tab = 'publications';
 
@@ -58,6 +66,46 @@ describe('SearchService', () => {
 
     http.expectOne({
       url: mockApiUrl + 'publication/_search?',
+      method: 'POST'
+    }).flush(searchResponseMock);
+  });
+
+  it('should send API request to funding/_search from fundings tab', () => {
+    const searchResponseMock = ResponseJsonFundings;
+    const http = TestBed.get(HttpTestingController);
+    const tabChangeService = TestBed.get(TabChangeService);
+    let searchResponse;
+
+    searchService.singleInput = 'searchtext';
+    searchService.fromPage = 1;
+    tabChangeService.tab = 'fundings';
+
+    searchService.getData().subscribe((response) => {
+      searchResponse = response;
+    });
+
+    http.expectOne({
+      url: mockApiUrl + 'funding/_search?',
+      method: 'POST'
+    }).flush(searchResponseMock);
+  });
+
+  it('should send API request to infrastructure/_search from infrastructures tab', () => {
+    const searchResponseMock = ResponseJsonInfrastructures;
+    const http = TestBed.get(HttpTestingController);
+    const tabChangeService = TestBed.get(TabChangeService);
+    let searchResponse;
+
+    searchService.singleInput = 'searchtext';
+    searchService.fromPage = 1;
+    tabChangeService.tab = 'infrastructures';
+
+    searchService.getData().subscribe((response) => {
+      searchResponse = response;
+    });
+
+    http.expectOne({
+      url: mockApiUrl + 'infrastructure/_search?',
       method: 'POST'
     }).flush(searchResponseMock);
   });
