da8ddd1fbfb8f5ddca8dda978bf950bec67e0f82
Péter Siket-Szász <peter.siket-szasz@csc.fi>
2020-07-01 13:42:53 +0300

    Added dynamic visualisation data query

--- src/app/services/filter.service.ts
+++ src/app/services/filter.service.ts
@@ -9,6 +9,7 @@ import { Injectable, Inject, LOCALE_ID  } from '@angular/core';
 import { SortService } from './sort.service';
 import { BehaviorSubject } from 'rxjs';
 import { SettingsService } from './settings.service';
+import { publication } from 'src/assets/static-data/visualisation.json';
 
 @Injectable({
   providedIn: 'root'
@@ -360,31 +361,51 @@ export class FilterService {
 
   constructVisualPayload(tab: string, searchTerm: string) {
     // Final query object
-    const res: any = {};
+    const res: any = {aggs: {}};
     // Order
     const order = {_key: 'desc'}
     // Create query with filters and search term
     const query = this.constructQuery(tab.slice(0, -1), searchTerm);
     // Query field hierarchy
-    const hierarchy = ['publicationYear', 'fields_of_science.nameFiScience.keyword', 'fields_of_science.fieldIdScience'];
-
-    // Populate query with aggregations
-    let q = res;
-    for (let i = 0; i < hierarchy.length; i++) {
-      // Get the next field
-      const s = hierarchy[i];
-      // Name aggregation hierarchy after field names
-      q = (i === 0) ? q : (q.aggs[hierarchy[i - 1]]);
-      console.log(q)
-      q.aggs = {};
-      q.aggs[s] = {terms: {field: s, size: 10, order: order}};
+    let hierarchies = publication;
+    // Get correct hierarchy based on tab
+    switch (tab) {
+      case 'publications':
+        hierarchies = publication;
+        break;
+    
+      default:
+        break;
     }
 
+    hierarchies.forEach(h => {
+      let agg: any = {};
+      let q = agg;
+
+      // Populate query with aggregations
+      for (let i = 0; i < h.hierarchy.length; i++) {
+        // Get the next field
+        const s = h.hierarchy[i];
+        // Name aggregation hierarchy after field names
+        q = (i === 0) ? q : (q.aggs[h.hierarchy[i - 1].name]);
+        // Add empty aggs
+        q.aggs = {};
+        // Add terms object
+        q.aggs[s.name] = {terms: {field: s.field, size: s.size}};
+        // Add order if needed
+        if (s.order) {
+          q.aggs[s.name].terms.order = order;
+        }
+      }
+      // Add second level of aggs to query
+      res.aggs[h.field] = agg.aggs[h.hierarchy[0].name];
+    })
+
+    // Add properties
     res.size = 0;
     res.query = query;
 
     return res;
-
   }
 
   constructFilterPayload(tab: string, searchTerm: string) {
