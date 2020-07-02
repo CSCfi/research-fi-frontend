cbb3165e531a09d04ef63371101e7aeb16af2a17
Timo Särkikoski <sarkikos@gmail.com>
2020-07-02 08:00:37 +0300

    Send Javascript errors to APM server

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,13 +21,16 @@ export class AppComponent {
 
   /*
   Application performance monitoring (APM) service must be initialized on component init.
-  APM Configuration parameters are taken from AppConfigService. 
+  Values for APM Configuration parameters 'serverUrl' and 'environment' are taken from AppConfigService.
+  https://www.elastic.co/guide/en/apm/agent/rum-js/current/configuration.html
   */
   constructor(@Inject(ApmService) service: ApmService, private appConfigService: AppConfigService) {
     const apm = service.init({
       serviceName: 'Angular',
       serverUrl: this.appConfigService.apmUrl,
       environment: this.appConfigService.environmentName,
+      eventsLimit: 10,
+      transactionSampleRate: 0.1,
       disableInstrumentations: [
         //'page-load',
         'history',
