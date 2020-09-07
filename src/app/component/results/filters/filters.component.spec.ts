7d79f4c27d6d97ce2f2fb0c5d039815c6a896f1d
saulipurhonen <sauli.purhonen@gmail.com>
2020-09-07 11:13:49 +0300

    Base for test

--- src/app/component/results/filters/filters.component.spec.ts
+++ src/app/component/results/filters/filters.component.spec.ts
@@ -1,7 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FiltersComponent } from './filters.component';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import { SortService } from '../../../services/sort.service';
 import { ResizeService } from '../../../services/resize.service';
 import { FilterService } from '../../../services/filter.service';
@@ -13,14 +13,32 @@ import { InfrastructureFilters } from './infrastructures';
 import { OrganizationFilters } from './organizations';
 import { NewsFilters } from './news';
 import { DataService } from 'src/app/services/data.service';
+import { WINDOW_PROVIDERS } from 'src/app/services/window.service';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap';
+import { ModalModule } from 'ngx-bootstrap';
+import { ActivatedRouteStub } from 'src/testing/activated-route-stub';
 
 describe('FiltersComponent', () => {
   let component: FiltersComponent;
   let fixture: ComponentFixture<FiltersComponent>;
 
+  const router = {
+    navigate: jasmine.createSpy('navigate')
+  };
+
+  const queryParamMock = ['testParam'];
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ FiltersComponent ]
+      declarations: [ FiltersComponent ],
+      providers: [
+        {provide: Router, useValue: router},
+        {provide: ActivatedRoute, useValue: new ActivatedRouteStub()},
+        WINDOW_PROVIDERS,
+      ],
+      imports: [
+        ModalModule.forRoot(),
+      ]
     })
     .compileComponents();
   }));
