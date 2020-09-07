1884ddf0b3646341b14cd5a25a9d5927bb52d355
saulipurhonen <sauli.purhonen@gmail.com>
2020-09-07 16:28:35 +0300

    Filter component tests and testdata

--- src/app/component/results/filters/filters.component.spec.ts
+++ src/app/component/results/filters/filters.component.spec.ts
@@ -18,21 +18,23 @@ import { BsModalRef, BsModalService } from 'ngx-bootstrap';
 import { ModalModule } from 'ngx-bootstrap';
 import { ActivatedRouteStub } from 'src/testing/activated-route-stub';
 
+import AggResponse from '../../../../testdata/aggregationresponse.json';
+
 describe('FiltersComponent', () => {
   let component: FiltersComponent;
   let fixture: ComponentFixture<FiltersComponent>;
 
-  const router = {
+  const routerSpy = {
     navigate: jasmine.createSpy('navigate')
   };
 
-  const queryParamMock = ['testParam'];
-
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ FiltersComponent ],
+      declarations: [
+        FiltersComponent,
+      ],
       providers: [
-        {provide: Router, useValue: router},
+        {provide: Router, useValue: routerSpy},
         {provide: ActivatedRoute, useValue: new ActivatedRouteStub()},
         WINDOW_PROVIDERS,
       ],
@@ -41,15 +43,68 @@ describe('FiltersComponent', () => {
       ]
     })
     .compileComponents();
-  }));
 
-  beforeEach(() => {
     fixture = TestBed.createComponent(FiltersComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    component.responseData = AggResponse;
+    component.activeFilters = {};
+  }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should add parameters to preselected filter category', () => {
+    component.activeFilters = Object.assign({}, {year: ['2020', '2019']});
+
+    component.selectionChange('year', '2021');
+
+    expect (routerSpy.navigate).toHaveBeenCalledWith([], {
+      queryParams: {
+        year: [ '2020', '2019', '2021' ],
+        sort: undefined,
+        page: 1 },
+        queryParamsHandling: 'merge'
+      }
+    );
+  });
+
+  it('should change year range accordingly', () => {
+    // Set from year to 2020 and expect query parameters to be in asceding order from chosen value to latest
+    const res = Object.assign({}, {value: 2020});
+
+    component.range(res, 'from');
+
+    expect (routerSpy.navigate).toHaveBeenCalledWith([], {
+      queryParams: {
+        year: [ '2021', '2020' ],
+        sort: undefined,
+        page: 1 },
+        queryParamsHandling: 'merge'
+      }
+    );
+  });
+
+  it('should select all from parent', () => {
+    component.responseData.aggregations.year.subData = AggResponse.aggregations.year.buckets;
+    const subFilter = component.responseData.aggregations.year;
+
+    component.selectAll('year', subFilter);
+
+    // Note: Year params are not in order because of previous test
+    expect (routerSpy.navigate).toHaveBeenCalledWith([], {
+      queryParams: {
+        year: [ '2020', '2019', '2021' ],
+        sort: undefined,
+        page: 1 },
+        queryParamsHandling: 'merge'
+      }
+    );
+  });
+
+  // it('should filter with term', () => {
+  //   const term = Object.assign({}, {event: {target: {value: 'test'}}});
+  //   console.log(term.event.target.value);
+  //   component.filterInput(term, 'testField');
+  // });
 });
