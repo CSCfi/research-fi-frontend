4efea0c7e7badc38336ffd2aab6e635aee9b9109
saulipurhonen <sauli.purhonen@gmail.com>
2020-09-08 15:52:55 +0300

    Filter organizations tests

---
+++ src/app/component/results/filters/organizations.spec.ts
@@ -0,0 +1,27 @@
+//  This file is part of the research.fi API service
+//
+//  Copyright 2019 Ministry of Education and Culture, Finland
+//
+//  :author: CSC - IT Center for Science Ltd., Espoo Finland servicedesk@csc.fi
+//  :license: MIT
+
+import { OrganizationFilters } from './organizations';
+import AggResponse from '../../../../testdata/aggregationresponse.json';
+
+describe('OrganizationFilters', () => {
+    let filter: OrganizationFilters;
+
+    beforeEach(() => {
+        filter = new OrganizationFilters();
+    });
+
+    it('should map data', () => {
+        const data = AggResponse.aggregations.testField;
+        // We need to mock correct fieldName and id that would occur in real data
+        const sectorName = 'sectorName';
+        data.testFields.buckets.map(item => {item[sectorName] = item.testFieldId, item.key = '1'; });
+
+        expect(filter.sector(AggResponse.aggregations.testField.testFields.buckets)).toBeDefined();
+    });
+
+  });
