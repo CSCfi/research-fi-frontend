395f06db63fd847784f7873058077e12e7b2ce5f
saulipurhonen <sauli.purhonen@gmail.com>
2020-09-10 14:29:11 +0300

    Added copyright

--- src/app/services/filters/news-filter.service.spec.ts
+++ src/app/services/filters/news-filter.service.spec.ts
@@ -1,3 +1,10 @@
+//  This file is part of the research.fi API service
+//
+//  Copyright 2019 Ministry of Education and Culture, Finland
+//
+//  :author: CSC - IT Center for Science Ltd., Espoo Finland servicedesk@csc.fi
+//  :license: MIT
+
 import { TestBed } from '@angular/core/testing';
 
 import { NewsFilterService } from './news-filter.service';
