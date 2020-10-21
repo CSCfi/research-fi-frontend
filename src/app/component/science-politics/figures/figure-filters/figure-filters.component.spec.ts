17b9e682d0606ad57babbce164ccf9bfd75f46f1
saulipurhonen <sauli.purhonen@gmail.com>
2020-10-21 11:09:14 +0300

    Simple test for navigation

--- src/app/component/science-politics/figures/figure-filters/figure-filters.component.spec.ts
+++ src/app/component/science-politics/figures/figure-filters/figure-filters.component.spec.ts
@@ -1,14 +1,18 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { Router } from '@angular/router';
 import { FigureFiltersComponent } from './figure-filters.component';
 
 describe('FigureFiltersComponent', () => {
   let component: FigureFiltersComponent;
   let fixture: ComponentFixture<FigureFiltersComponent>;
+  const routerSpy = {navigate: jasmine.createSpy('navigate')};
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ FigureFiltersComponent ]
+      declarations: [ FigureFiltersComponent ],
+      providers: [
+        { provide: Router, useValue: routerSpy }
+      ]
     })
     .compileComponents();
   }));
@@ -21,5 +25,14 @@ describe('FigureFiltersComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should navigate with params', () => {
+    component.navigate('test');
+    expect (routerSpy.navigate).toHaveBeenCalledWith([], {
+      queryParams: {
+        filter: 'test',
+      },
+    });
   });
 });
