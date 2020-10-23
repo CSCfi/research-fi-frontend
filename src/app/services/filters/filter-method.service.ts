1536402105dcb618e34dcd8c2710305ce30a5ab3
saulipurhonen <sauli.purhonen@gmail.com>
2020-10-23 14:01:19 +0300

    Mapped minor field ids to string type

--- src/app/services/filters/filter-method.service.ts
+++ src/app/services/filters/filter-method.service.ts
@@ -23,7 +23,7 @@ export class FilterMethodService {
     // Map fields by field & nested id
     if (source && source.length > 0) {
       mapped = source.map(majorField => ({
-        key: majorField.fieldId.buckets[0]?.key || - 1,
+        key: majorField.fieldId.buckets[0]?.key.toString() || - 1,
         label: majorField.key,
         // Invalid response if key is 0
         id: majorField.fieldId.buckets[0]?.key || - 1,
