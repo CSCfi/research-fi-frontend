b6c457c86184cae0a675f35f8a9a63c4cfbcc72a
saulipurhonen <sauli.purhonen@gmail.com>
2020-11-20 15:24:20 +0200

    Method for figure data

--- src/app/services/content-data.service.ts
+++ src/app/services/content-data.service.ts
@@ -8,7 +8,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/internal/Observable';
-import { Shortcuts, ShortcutsAdapter } from '../models/shortcuts.model';
+import { Shortcut, ShortcutAdapter } from '../models/shortcut.model';
+import { Figure, FigureAdapter } from '../models/figure/figure.model';
 import { map } from 'rxjs/operators';
 
 @Injectable({
@@ -16,13 +17,21 @@ import { map } from 'rxjs/operators';
 })
 export class ContentDataService {
   apiUrl: string;
-  constructor(private http: HttpClient, private shortcutsAdapter: ShortcutsAdapter) {
+  constructor(private http: HttpClient, private shortcutAdapter: ShortcutAdapter, private figureAdapter: FigureAdapter) {
     this.apiUrl = 'http://127.0.0.1:8000/apis/v1/';
    }
 
-  // Get data from backend
-  getData(source): Observable<Shortcuts[]> {
-    return this.http.get<Shortcuts[]>(this.apiUrl + source + '/')
-    .pipe(map(data => this.shortcutsAdapter.adaptMany(data)));
+  /*
+   * Get data from backend
+   */
+
+  getShortcuts(): Observable<Shortcut[]> {
+    return this.http.get<Shortcut[]>(this.apiUrl + 'shortcuts/')
+    .pipe(map(data => this.shortcutAdapter.adaptMany(data)));
+  }
+
+  getFigures(): Observable<Figure[]> {
+    return this.http.get<Figure[]>(this.apiUrl + 'figure/')
+    .pipe(map(data => this.figureAdapter.adaptMany(data)));
   }
 }
