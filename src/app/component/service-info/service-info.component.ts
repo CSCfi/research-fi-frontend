2c5d3ee5477025d100fced3d17bbe90e8b24cbdb
saulipurhonen <sauli.purhonen@gmail.com>
2020-11-24 15:14:40 +0200

    Content from API

--- src/app/component/service-info/service-info.component.ts
+++ src/app/component/service-info/service-info.component.ts
@@ -6,6 +6,7 @@ import { TabChangeService } from 'src/app/services/tab-change.service';
 import { Location } from '@angular/common';
 import { UtilityService } from 'src/app/services/utility.service';
 import { serviceInfo, common } from 'src/assets/static-data/meta-tags.json';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-service-info',
@@ -24,14 +25,20 @@ export class ServiceInfoComponent implements OnInit, AfterViewInit, OnDestroy {
 
   private metaTags = serviceInfo;
   private commonTags = common;
+  content: any[];
 
   constructor(private titleService: Title, @Inject(LOCALE_ID) protected localeId: string, private tabChangeService: TabChangeService,
-              private location: Location, private utilityService: UtilityService) {
+              private location: Location, private utilityService: UtilityService, private route: ActivatedRoute) {
     // Capitalize first letter of locale
     this.currentLocale = this.localeId.charAt(0).toUpperCase() + this.localeId.slice(1);
   }
 
   ngOnInit(): void {
+    // Get page data. Data is passed with resolver in router
+    const pageData = this.route.snapshot.data.pages;
+    this.content = pageData.filter(el => el.id.includes('service-info'));
+    console.log(this.content);
+
     this.utilityService.addMeta(this.metaTags['title' + this.currentLocale],
                                 this.metaTags['description' + this.currentLocale],
                                 this.commonTags['imgAlt' + this.currentLocale])
