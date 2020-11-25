e3e30c30519f5c3d035d6230d4415ce98c69a391
saulipurhonen <sauli.purhonen@gmail.com>
2020-11-25 12:04:02 +0200

    Changed comment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -53,7 +53,7 @@ export class AppComponent implements OnInit {
     this.routerEvents();
   }
 
-  // Set loading indicator. This useful for pages that rely on resolvers
+  // Set loading indicator. This is useful for pages that rely on resolvers
   routerEvents() {
     this.router.events.subscribe((event: RouterEvent) => {
       switch (true) {
