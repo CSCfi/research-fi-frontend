c0b2a43d8057a39b37f529fa6abdaf740d72135f
saulipurhonen <sauli.purhonen@gmail.com>
2020-12-14 16:45:51 +0200

    Push data to session storage to reduce API calls

--- src/app/resolvers/research-innovation-system-sector-resolver.service.ts
+++ src/app/resolvers/research-innovation-system-sector-resolver.service.ts
@@ -11,6 +11,7 @@ import {
   ActivatedRouteSnapshot,
   RouterStateSnapshot
 } from '@angular/router';
+import { tap } from 'rxjs/operators';
 import { ContentDataService } from '../services/content-data.service';
 
 @Injectable({
@@ -21,6 +22,11 @@ export class ResearchInnovationSystemSectorResolver implements Resolve<any> {
   constructor( private cds: ContentDataService) { }
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    return this.cds.getSectors();
+    // Prevent API call if data exists in session storage
+    if (!sessionStorage.getItem('sectorData')) {
+      return this.cds.getSectors().pipe(tap(data => sessionStorage.setItem('sectorData', JSON.stringify(data))));
+    } else {
+      return JSON.parse(sessionStorage.getItem('sectorData'));
+    }
   }
 }
