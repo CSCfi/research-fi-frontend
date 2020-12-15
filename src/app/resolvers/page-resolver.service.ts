34179350073c0d991b4a386982e20c28ab788563
saulipurhonen <sauli.purhonen@gmail.com>
2020-12-15 13:40:55 +0200

    Resolver fallback far SSR. SSR can't use session storage, use API call as fallback

--- src/app/resolvers/page-resolver.service.ts
+++ src/app/resolvers/page-resolver.service.ts
@@ -5,7 +5,8 @@
 //  :author: CSC - IT Center for Science Ltd., Espoo Finland servicedesk@csc.fi
 //  :license: MIT
 
-import { Injectable } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
 import {
   Resolve,
   ActivatedRouteSnapshot,
@@ -19,14 +20,18 @@ import { ContentDataService } from '../services/content-data.service';
 })
 export class PageResolverService implements Resolve<any> {
 
-  constructor( private cds: ContentDataService) { }
+  constructor( private cds: ContentDataService, @Inject(PLATFORM_ID) private platformId: object) { }
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
     // Prevent API call if data exists in session storage
-    if (!sessionStorage.getItem('pageData')) {
-      return this.cds.getPages().pipe(tap(data => sessionStorage.setItem('pageData', JSON.stringify(data))));
+    if (isPlatformBrowser(this.platformId)) {
+      if (!sessionStorage.getItem('pageData')) {
+        return this.cds.getPages().pipe(tap(data => sessionStorage.setItem('pageData', JSON.stringify(data))));
+      } else {
+        return JSON.parse(sessionStorage.getItem('pageData'));
+      }
     } else {
-      return JSON.parse(sessionStorage.getItem('pageData'));
+      return this.cds.getPages();
     }
   }
 }
--- src/app/resolvers/shortcut-resolver.service.ts
+++ src/app/resolvers/shortcut-resolver.service.ts
@@ -5,7 +5,8 @@
 //  :author: CSC - IT Center for Science Ltd., Espoo Finland servicedesk@csc.fi
 //  :license: MIT
 
-import { Injectable } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
 import {
   Resolve,
   ActivatedRouteSnapshot,
@@ -19,14 +20,18 @@ import { ContentDataService } from '../services/content-data.service';
 })
 export class ShortcutResolverService implements Resolve<any> {
 
-  constructor( private cds: ContentDataService) { }
+  constructor( private cds: ContentDataService, @Inject(PLATFORM_ID) private platformId: object) { }
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
     // Prevent API call if data exists in session storage
-    if (!sessionStorage.getItem('shortcutData')) {
-      return this.cds.getShortcuts().pipe(tap(data => sessionStorage.setItem('shortcutData', JSON.stringify(data))));
+    if (isPlatformBrowser(this.platformId)) {
+      if (!sessionStorage.getItem('shortcutData')) {
+        return this.cds.getShortcuts().pipe(tap(data => sessionStorage.setItem('shortcutData', JSON.stringify(data))));
+      } else {
+        return JSON.parse(sessionStorage.getItem('shortcutData'));
+      }
     } else {
-      return JSON.parse(sessionStorage.getItem('shortcutData'));
+      return this.cds.getShortcuts();
     }
   }
 }
