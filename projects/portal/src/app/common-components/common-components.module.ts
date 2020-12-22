584517cdae0abcd38bbe058760d6375c91821033
saulipurhonen <sauli.purhonen@gmail.com>
2020-12-22 14:08:16 +0200

    Moved buttons to library

--- projects/portal/src/app/common-components/common-components.module.ts
+++ projects/portal/src/app/common-components/common-components.module.ts
@@ -2,20 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InfoComponent } from './info/info.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { SecondaryButtonComponent } from './secondary-button/secondary-button.component';
-import { PrimaryActionButtonComponent } from './primary-action-button/primary-action-button.component';
-import { CloseButtonComponent } from './close-button/close-button.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
 
 
 @NgModule({
-  declarations: [InfoComponent, SecondaryButtonComponent, PrimaryActionButtonComponent, CloseButtonComponent, NotFoundComponent],
+  declarations: [InfoComponent, NotFoundComponent],
   exports: [
     InfoComponent,
-    SecondaryButtonComponent,
-    PrimaryActionButtonComponent,
-    CloseButtonComponent
   ],
   imports: [
     CommonModule,
