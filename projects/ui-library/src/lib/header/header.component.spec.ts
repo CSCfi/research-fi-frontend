bac8ebbf24771bdc08dd881a366ecae048ba20c1
saulipurhonen <sauli.purhonen@gmail.com>
2020-12-22 13:09:59 +0200

    Added copyright notice

--- projects/ui-library/src/lib/header/header.component.spec.ts
+++ projects/ui-library/src/lib/header/header.component.spec.ts
@@ -1,3 +1,11 @@
+// This file is part of the research.fi API service
+//
+// Copyright 2019 Ministry of Education and Culture, Finland
+//
+// :author: CSC - IT Center for Science Ltd., Espoo Finland servicedesk@csc.fi
+// :license: MIT
+
+
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeaderComponent } from './header.component';
