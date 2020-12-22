200ae5829540f43f4df40b32b29dabc5e64bc49b
saulipurhonen <sauli.purhonen@gmail.com>
2020-12-22 08:30:23 +0200

    Window provider

--- projects/ui-library/src/lib/ui-library.module.ts
+++ projects/ui-library/src/lib/ui-library.module.ts
@@ -1,13 +1,26 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { UiLibraryComponent } from './ui-library.component';
 import { HeaderComponent } from './header/header.component';
-
-
+import { BrowserModule } from '@angular/platform-browser';
+import { CloseButtonComponent } from './buttons/close-button/close-button.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { ClickOutsideModule } from 'ng-click-outside';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { WINDOW_PROVIDERS } from './services/window.service';
 
 @NgModule({
-  declarations: [UiLibraryComponent, HeaderComponent],
+  declarations: [UiLibraryComponent, HeaderComponent, CloseButtonComponent],
   imports: [
+    RouterModule,
+    BrowserModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    ClickOutsideModule,
+    FontAwesomeModule,
   ],
-  exports: [UiLibraryComponent]
+  exports: [UiLibraryComponent, HeaderComponent],
+  providers: [WINDOW_PROVIDERS],
 })
 export class UiLibraryModule { }
