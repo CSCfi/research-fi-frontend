59482c9ba8e54e1f8b4b1baa867046e30a6c97f3
saulipurhonen <sauli.purhonen@gmail.com>
2020-12-22 12:41:28 +0200

    Hide html body overflow when mobile menu is open

--- projects/ui-library/src/lib/header/header.component.ts
+++ projects/ui-library/src/lib/header/header.component.ts
@@ -1,5 +1,22 @@
-import { ElementRef, Inject, OnDestroy, ViewChild } from '@angular/core';
-import { Component, Input, OnInit } from '@angular/core';
+// This file is part of the research.fi API service
+//
+// Copyright 2019 Ministry of Education and Culture, Finland
+//
+// :author: CSC - IT Center for Science Ltd., Espoo Finland servicedesk@csc.fi
+// :license: MIT
+
+import {
+  Component,
+  Input,
+  OnInit,
+  ElementRef,
+  HostListener,
+  Inject,
+  OnDestroy,
+  ViewChild,
+  ViewEncapsulation,
+} from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 import { Subscription } from 'rxjs/internal/Subscription';
 import { ResizeService } from '../services/resize.service';
 import { WINDOW } from '../services/window.service';
@@ -8,6 +25,7 @@ import { WINDOW } from '../services/window.service';
   selector: 'lib-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class HeaderComponent implements OnInit, OnDestroy {
   @Input() navItems: any;
@@ -23,28 +41,42 @@ export class HeaderComponent implements OnInit, OnDestroy {
   consent: string;
   private resizeSub: Subscription;
   @ViewChild('mainNavbar', { static: true }) mainNavbar: ElementRef;
+  @ViewChild('navbarToggler', { static: true }) navbarToggler: ElementRef;
 
   constructor(
     private resizeService: ResizeService,
     @Inject(WINDOW) private window: Window,
-    ) {}
+    @Inject(DOCUMENT) private document: any
+  ) {}
 
   ngOnInit(): void {
+    this.lang = this.currentLang.toUpperCase();
     this.resizeSub = this.resizeService.onResize$.subscribe((dims) =>
       this.onResize(dims)
     );
   }
 
+  @HostListener('document:keydown.escape', ['$event'])
+  escapeListener(event: any) {
+    if (this.mobile && this.navbarOpen) {
+      this.toggleNavbar();
+      setTimeout(() => {
+        this.navbarToggler?.nativeElement.focus();
+      }, 1);
+    }
+  }
+
   toggleNavbar() {
     this.navbarOpen = !this.navbarOpen;
+    this.navbarOpen
+      ? (this.document.body.style.overflow = 'hidden')
+      : (this.document.body.style.overflow = 'visible');
 
     // Allow menu to slide out before hiding
     setTimeout(() => {
       this.hideOverflow = !this.hideOverflow;
     }, 250 * (1 - +this.navbarOpen));
   }
-
-  setLang(lang: string) {}
 
   onClickedOutside(e: Event) {
     this.dropdownOpen = false;
