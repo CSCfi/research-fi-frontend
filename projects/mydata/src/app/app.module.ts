2ccdc51e7259c0a919013e867f47b5d04bf75bd6
saulipurhonen <sauli.purhonen@gmail.com>
2020-12-30 10:49:00 +0200

    Import material checkbox

--- projects/mydata/src/app/app.module.ts
+++ projects/mydata/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -9,22 +10,28 @@ import { UiLibraryModule } from 'ui-library';
 import { LayoutModule } from './layout/layout.module';
 
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+
 import { HomeComponent } from './components/home/home.component';
 import { IntroductionComponent } from './components/introduction/introduction.component';
+import { WelcomeStepperComponent } from './components/welcome-stepper/welcome-stepper.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    IntroductionComponent
+    IntroductionComponent,
+    WelcomeStepperComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     UiLibraryModule,
     LayoutModule,
     MatProgressSpinnerModule,
+    MatCheckboxModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
