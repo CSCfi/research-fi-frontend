bac6844af32d20409e27894bd90e48774a3a8a51
saulipurhonen <sauli.purhonen@gmail.com>
2020-12-30 15:28:05 +0200

    Handler for orcid data

---
+++ projects/mydata/src/app/components/welcome-stepper/orcid-data-handler/orcid-data-handler.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { OrcidDataHandlerComponent } from './orcid-data-handler.component';
+
+describe('OrcidDataHandlerComponent', () => {
+  let component: OrcidDataHandlerComponent;
+  let fixture: ComponentFixture<OrcidDataHandlerComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ OrcidDataHandlerComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(OrcidDataHandlerComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
