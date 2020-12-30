1b445de5e252ad38d9093c2df9704716e1022024
saulipurhonen <sauli.purhonen@gmail.com>
2020-12-30 10:50:01 +0200

    Welcome stepper

---
+++ projects/mydata/src/app/components/welcome-stepper/welcome-stepper.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { WelcomeStepperComponent } from './welcome-stepper.component';
+
+describe('WelcomeStepperComponent', () => {
+  let component: WelcomeStepperComponent;
+  let fixture: ComponentFixture<WelcomeStepperComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ WelcomeStepperComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(WelcomeStepperComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
