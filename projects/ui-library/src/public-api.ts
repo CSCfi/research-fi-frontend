99847c43f88041c2b138dda900ef5ac59febe8aa
saulipurhonen <sauli.purhonen@gmail.com>
2020-12-31 11:36:11 +0200

    Export services

--- projects/ui-library/src/public-api.ts
+++ projects/ui-library/src/public-api.ts
@@ -2,6 +2,8 @@
  * Public API Surface of ui-library
  */
 
+export * from './lib/services/resize.service';
+export * from './lib/services/window.service';
 export * from './lib/buttons/close-button/close-button.component';
 export * from './lib/buttons/primary-action-button/primary-action-button.component';
 export * from './lib/buttons/secondary-button/secondary-button.component';
