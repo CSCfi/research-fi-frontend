e7db26fc3ea4813b45502f464c279a04805dc42c
Sauli Purhonen <sauli.purhonen@gmail.com>
2021-02-25 12:35:26 +0200

    Portal routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,10 +14,11 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LayoutModule } from './layout/layout.module';
-// import { AppConfigService } from './shared/services/app-config-service.service';
-// import { CommonComponentsModule } from './shared/shared.module';
-// import { PortalModule } from './portal/portal.module';
-// import { InterceptService } from './shared/services/intercept.service';
+import { AppConfigService } from './shared/services/app-config-service.service';
+import { CommonComponentsModule } from './shared/shared.module';
+import { InterceptService } from './shared/services/intercept.service';
+import { PortalModule } from './portal/portal.module';
+import { PortalRoutingModule } from './portal/portal-routing.module';
 
 @NgModule({
   declarations: [
@@ -25,32 +26,33 @@ import { LayoutModule } from './layout/layout.module';
   ],
   imports: [
     BrowserModule,
-    // BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
     HttpClientModule,
     // AppRoutingModule,
     BrowserAnimationsModule,
     LayoutModule,
-    // CommonComponentsModule,
-    // PortalModule,
+    CommonComponentsModule,
+    PortalModule,
+    PortalRoutingModule,
   ],
   providers: [
-    // AppConfigService,
-    // {
-    //   provide: APP_INITIALIZER,
-    //   multi: true,
-    //   deps: [AppConfigService],
-    //   useFactory: (appConfigService: AppConfigService) => {
-    //     // Load configuration from file when application starts.
-    //     return () => {
-    //       return appConfigService.loadAppConfig();
-    //     };
-    //   },
-    // },
-    // {
-    //   provide: HTTP_INTERCEPTORS,
-    //   useClass: InterceptService,
-    //   multi: true,
-    // }
+    AppConfigService,
+    {
+      provide: APP_INITIALIZER,
+      multi: true,
+      deps: [AppConfigService],
+      useFactory: (appConfigService: AppConfigService) => {
+        // Load configuration from file when application starts.
+        return () => {
+          return appConfigService.loadAppConfig();
+        };
+      },
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: InterceptService,
+      multi: true,
+    }
   ],
   bootstrap: [AppComponent]
 })
