b8bff4fff50a00ac092828d4de770a5924f1ec39
Sauli Purhonen <sauli.purhonen@gmail.com>
2021-02-26 13:15:52 +0200

    Loading indicator logic to layout module

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -6,14 +6,46 @@
 // # :license: MIT
 
 import { Component, OnInit } from '@angular/core';
+import {
+  Router,
+  RouterEvent,
+  NavigationStart,
+  NavigationEnd,
+} from '@angular/router';
+import { AuthService } from '../mydata/services/auth.service';
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.scss'],
 })
-export class LayoutComponent implements OnInit {
-  constructor() {}
 
-  ngOnInit() {}
+/*
+  We want to show loading indicator before data is resolved from CMS
+  amd when user initializes authorization process.
+*/
+export class LayoutComponent implements OnInit {
+  loading: boolean;
+  authInitialized = this.authService.authInitialized;
+
+  constructor(private router: Router, private authService: AuthService) {}
+
+  ngOnInit() {
+    this.routerEvents();
+  }
+
+  routerEvents() {
+    this.router.events.subscribe((event: RouterEvent) => {
+      switch (true) {
+        case event instanceof NavigationStart: {
+          this.loading = true;
+          break;
+        }
+        case event instanceof NavigationEnd: {
+          this.loading = false;
+          break;
+        }
+      }
+    });
+  }
 }
