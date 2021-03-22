5187d9e3dc238ac84916d9e6755434048da88c05
Sauli Purhonen <sauli.purhonen@gmail.com>
2021-03-22 14:43:58 +0200

    Wrap login process in platform check

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,9 @@
 //  :author: CSC - IT Center for Science Ltd., Espoo Finland servicedesk@csc.fi
 //  :license: MIT
 
-import { Component } from '@angular/core';
+import { Component, Inject, PLATFORM_ID } from '@angular/core';
 import { OidcSecurityService } from 'angular-auth-oidc-client';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -16,11 +17,16 @@ import { OidcSecurityService } from 'angular-auth-oidc-client';
 export class AppComponent {
   title = 'research-fi-portal';
 
-  constructor(public oidcSecurityService: OidcSecurityService) {
-    this.oidcSecurityService
-      .checkAuth()
-      .subscribe((isAuthenticated) =>
-        console.log('app authenticated', isAuthenticated)
-      );
+  constructor(
+    public oidcSecurityService: OidcSecurityService,
+    @Inject(PLATFORM_ID) private platformId: object
+  ) {
+    if (isPlatformBrowser(this.platformId)) {
+      this.oidcSecurityService
+        .checkAuth()
+        .subscribe((isAuthenticated) =>
+          console.log('app authenticated', isAuthenticated)
+        );
+    }
   }
 }
