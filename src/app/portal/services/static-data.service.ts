ea2ab0dba50f6431958c6cdb45e2ae0087d8fe59
Péter Siket-Szász <peter.siket-szasz@csc.fi>
2021-03-24 11:17:30 +0200

    Moved dataset organization fields to nested fields

--- src/app/portal/services/static-data.service.ts
+++ src/app/portal/services/static-data.service.ts
@@ -429,13 +429,6 @@ export class StaticDataService {
           'descriptionEn',
           'descriptionSv',
           'datasetCreated',
-          'actor.sector.organization.organizationNameFi',
-          'actor.sector.organization.organizationNameEn',
-          'actor.sector.organization.organizationNameSv',
-          'actor.sector.organization.organizationUnit.organizationUnitNameFi',
-          'actor.sector.organization.organizationUnit.organizationUnitNameEn',
-          'actor.sector.organization.organizationUnit.organizationUnitNameSv',
-          'actor.sector.organization.organizationUnit.person.authorFullName',
           'fieldsOfScience.nameFiScience',
           'fieldsOfScience.nameEnScience',
           'fieldsOfScience.nameSvScience',
@@ -534,6 +527,19 @@ export class StaticDataService {
           'fundingGroupPerson.roleInFundingGroup',
           'fundingGroupPerson.consortiumProject',
         ];
+        break;
+      }
+      case 'dataset': {
+        res = [
+          'actor.sector.organization.OrganizationNameFi',
+          'actor.sector.organization.OrganizationNameEn',
+          'actor.sector.organization.OrganizationNameSv',
+          'actor.sector.organization.organizationUnit.organizationUnitNameFi',
+          'actor.sector.organization.organizationUnit.organizationUnitNameEn',
+          'actor.sector.organization.organizationUnit.organizationUnitNameSv',
+          'actor.sector.organization.organizationUnit.person.authorFullName'
+        ];
+        break;
       }
     }
     return res;
@@ -707,9 +713,9 @@ export class StaticDataService {
           }
           case 'dataset': {
             res = [
-              'actor.sector.organization.organizationNameFi',
-              'actor.sector.organization.organizationNameEn',
-              'actor.sector.organization.organizationNameSv',
+              'actor.sector.organization.OrganizationNameFi',
+              'actor.sector.organization.OrganizationNameEn',
+              'actor.sector.organization.OrganizationNameSv',
             ];
             break;
           }
@@ -807,6 +813,12 @@ export class StaticDataService {
             ];
             break;
           }
+          case 'dataset': {
+            res = [
+              'actor.sector.organization.organizationUnit.person.authorFullName',
+            ];
+            break;
+          }
         }
         break;
       }
@@ -828,6 +840,14 @@ export class StaticDataService {
               'fundingGroupPerson.consortiumOrganizationNameFi',
               'fundingGroupPerson.consortiumOrganizationNameEn',
               'fundingGroupPerson.consortiumOrganizationNameSv',
+            ];
+            break;
+          }
+          case 'dataset': {
+            res = [
+              'actor.sector.organization.organizationNameFi',
+              'actor.sector.organization.organizationNameEn',
+              'actor.sector.organization.organizationNameSv'
             ];
             break;
           }
