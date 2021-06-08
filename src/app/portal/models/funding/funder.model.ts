326b2a6e42faaf26cab04130848294ac5cec7c4b
Péter Siket-Szász <peter.siket-szasz@csc.fi>
2021-06-08 14:32:14 +0300

    Restored funder name logic, only change source for structural funds

--- src/app/portal/models/funding/funder.model.ts
+++ src/app/portal/models/funding/funder.model.ts
@@ -43,7 +43,7 @@ export class FunderAdapter implements Adapter<Funder> {
     });
 
     return new Funder(
-      item.structuralFund ? 'Rakennerahastotietopalvelu' : this.lang.testLang('funderName', item),
+      this.lang.testLang('funderName', item),
       item.funderNameUnd,
       item.typeOfFundingId,
       this.lang.testLang('typeOfFundingName', item).trim().length > 0
