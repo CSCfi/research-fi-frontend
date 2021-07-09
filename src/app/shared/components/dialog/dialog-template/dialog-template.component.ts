e13f5c56e469ed51ae5e6ac1c53bb04f0d955414
Sauli Purhonen <sauli.purhonen@gmail.com>
2021-07-09 08:47:13 +0300

    Reusable dialog

--- src/app/shared/components/dialog/dialog-template/dialog-template.component.ts
+++ src/app/shared/components/dialog/dialog-template/dialog-template.component.ts
@@ -1,6 +1,7 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { AppSettingsService } from '@shared/services/app-settings.service';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-dialog-template',
@@ -8,13 +9,19 @@ import { AppSettingsService } from '@shared/services/app-settings.service';
   styleUrls: ['./dialog-template.component.scss'],
 })
 export class DialogTemplateComponent implements OnInit {
+  mobile: boolean;
+
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: any,
     private dialogRef: MatDialogRef<DialogTemplateComponent>,
     private appSettingsService: AppSettingsService
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.appSettingsService.mobileStatus
+      .pipe(take(1))
+      .subscribe((status) => (this.mobile = status));
+  }
 
   doAction(method) {
     this.dialogRef.close({
