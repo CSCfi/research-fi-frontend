193d1dcb88d79f11b4db9f4164d241929b219554
Sauli Purhonen <sauli.purhonen@visma.com>
2021-10-27 08:58:19 +0300

    Profile summary layout updates

---
+++ src/app/mydata/components/profile/profile-summary/summary-affiliation/summary-affiliation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-summary-affiliation',
+  templateUrl: './summary-affiliation.component.html',
+  styleUrls: ['./summary-affiliation.component.scss']
+})
+export class SummaryAffiliationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
