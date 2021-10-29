21ee2b32634f755a2ab6203e2b7d89b2735f7f88
Sauli Purhonen <sauli.purhonen@visma.com>
2021-10-29 14:25:04 +0300

    Description keywords fix

--- src/app/shared/services/app-settings.service.ts
+++ src/app/shared/services/app-settings.service.ts
@@ -79,7 +79,7 @@ export class AppSettingsService {
 
   myDataSettings = {
     develop: false,
-    debug: false,
+    debug: true,
     beta: true,
     appName: 'myData',
     label: 'Tutkijan tiedot',
