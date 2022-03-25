d0d67ec8d770c26188c758d82ba48596cf4e8fca
Kari Konola <kari.konola@csc.fi>
2022-03-25 19:12:38 +0200

    work stashed. news filter modal broken

--- src/app/portal/components/news/news-card/news-card.component.ts
+++ src/app/portal/components/news/news-card/news-card.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
 import { News } from 'src/app/portal/models/news.model';
+import { FundingCall } from '@portal/models/funding-call.model';
 
 @Component({
   selector: 'app-news-card',
@@ -8,14 +9,22 @@ import { News } from 'src/app/portal/models/news.model';
   encapsulation: ViewEncapsulation.None,
 })
 export class NewsCardComponent implements OnInit {
+  @Input() itemTypeFundingCalls = false;
   @Input() item: News;
+  @Input() set fundingCall(fundingCall: FundingCall) {
+    this._fundingCall = fundingCall;
+  }
+
   @Input() dense: boolean;
   @Input() isHomepage = false;
   @Input() term: string;
+
+  public _fundingCall: FundingCall;
 
   constructor() {
     this.term = this.term?.replace(/ä/g, '&auml;').replace(/ö/g, '&ouml;');
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+  }
 }
