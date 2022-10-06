911c9cdcc34f786cd3db53a127b3320707a7c6bb
Sauli Purhonen <sauli.purhonen@visma.com>
2022-10-06 09:13:31 +0300

    Changes to deployment cancellation

--- src/app/mydata/components/service-deployment/cancel-deployment/cancel-deployment.component.ts
+++ src/app/mydata/components/service-deployment/cancel-deployment/cancel-deployment.component.ts
@@ -1,6 +1,10 @@
+import { HttpResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ProfileService } from '@mydata/services/profile.service';
 import { UtilityService } from '@shared/services/utility.service';
+import { OidcSecurityService } from 'angular-auth-oidc-client';
+import { take } from 'rxjs';
 
 @Component({
   selector: 'app-cancel-deployment',
@@ -8,7 +12,12 @@ import { UtilityService } from '@shared/services/utility.service';
 })
 export class CancelDeploymentComponent implements OnInit {
   previousStep: number;
-  constructor(private router: Router, private utilityService: UtilityService) {
+  constructor(
+    private router: Router,
+    private utilityService: UtilityService,
+    private oidcSecurityService: OidcSecurityService,
+    private profileService: ProfileService
+  ) {
     // Look for previous step
     this.previousStep =
       this.router.getCurrentNavigation().previousNavigation?.finalUrl.queryParams?.step;
@@ -18,6 +27,25 @@ export class CancelDeploymentComponent implements OnInit {
     this.utilityService.setMyDataTitle(
       $localize`:@@cancelServiceDeployment:Peruutetaanko palvelun käyttöönotto?`
     );
+  }
+
+  cancelDeployment() {
+    const token = this.oidcSecurityService.getAccessToken();
+
+    const navigate = () => this.router.navigate(['/mydata']);
+
+    if (token) {
+      this.profileService
+        .deleteAccount()
+        .pipe(take(1))
+        .subscribe((res: HttpResponse<any>) => {
+          if (res.ok) {
+            navigate();
+          }
+        });
+    } else {
+      navigate();
+    }
   }
 
   continueDeployment() {
