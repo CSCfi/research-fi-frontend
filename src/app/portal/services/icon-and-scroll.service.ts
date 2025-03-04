d24b97027f9820fa63f5ce3221e7b9310bbf09cc
konolak <kari.konola@csc.fi>
2025-03-04 18:08:58 +0200

    implement position memory in search results scrolling

--- src/app/portal/services/icon-and-scroll.service.ts
+++ src/app/portal/services/icon-and-scroll.service.ts
@@ -1,8 +1,8 @@
-import { Injectable, OnDestroy } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { Inject, Injectable, OnDestroy, PLATFORM_ID } from '@angular/core';
+import { BehaviorSubject, Subscription } from 'rxjs';
 import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
-import { Event, Router, Scroll } from '@angular/router';
-import { ViewportScroller } from '@angular/common';
+import { Event, NavigationEnd, Router, Scroll } from '@angular/router';
+import { isPlatformBrowser, ViewportScroller } from '@angular/common';
 import { HistoryService } from '@portal/services/history.service';
 import { TabChangeService } from '@portal/services/tab-change.service';
 import { filter } from 'rxjs/operators';
@@ -11,9 +11,12 @@ import { faExternalLinkAlt, faInfoCircle } from '@fortawesome/free-solid-svg-ico
 @Injectable({
   providedIn: 'root'
 })
-export class MysteryService implements OnDestroy {
+export class IconAndScrollService implements OnDestroy {
   startPage;
+  resultsPageScrollPositionY = -1;
   routeSub: Subscription;
+
+  private isContentFetched = new BehaviorSubject(false);
 
   isResultPage(url: string) {
     // Check if the page is on results, and that the tabname ends with 's' (not single result)
@@ -45,30 +48,51 @@ export class MysteryService implements OnDestroy {
     }
   }
 
+
+  notifyContentFetched() {
+    this.isContentFetched.next(true);
+  }
+
   constructor(
     library: FaIconLibrary,
     router: Router,
     viewportScroller: ViewportScroller,
     private historyService: HistoryService,
-    private tabChangeService: TabChangeService
+    private tabChangeService: TabChangeService,
+    @Inject(PLATFORM_ID) private platformId: object
   ) {
+    this.isContentFetched.subscribe(isContentFetched => {
+      if (isContentFetched && this.resultsPageScrollPositionY !== -1) {
+        resultsPageMemoryScroll();
+      }
+    });
+
+    // Add global icons
+    library.addIcons(faExternalLinkAlt as any, faInfoCircle as any);
+
     this.startPage = router.parseUrl(router.url).queryParams.page || 1;
 
     // Scroll to top of page
     // Timeout value of 0 helps Firefox to scroll
     const scrollToTop = () => {
-      console.log("XXX scrollToTop");
+      //console.log("XXX scrollToTop");
+      setTimeout(() => viewportScroller.scrollToPosition([0, 0]), 10);
+    };
 
-      setTimeout(() => viewportScroller.scrollToPosition([0, 0]), 0);
+    const resultsPageMemoryScroll = () => {
+      if (isPlatformBrowser(this.platformId)) {
+        setTimeout(() => {
+          viewportScroller.scrollToPosition([0, this.resultsPageScrollPositionY]);
+          this.resultsPageScrollPositionY = -1;
+        }, 0);
+      }
     };
 
     // Used to prevent scroll to top when filters are selected
     this.routeSub = router.events
-      .pipe(filter((e: Event): e is Scroll => e instanceof Scroll))
+      .pipe(filter((e: Event): e is NavigationEnd => e instanceof NavigationEnd))
       .subscribe((e) => {
-        console.log("XXX routeSub fires");
-
-        const currentUrl = e.routerEvent.url;
+        const currentUrl = e.url;
         const history = this.historyService.history;
         const resultPages = tabChangeService.tabData
           .map((tab) => tab.link)
@@ -77,6 +101,7 @@ export class MysteryService implements OnDestroy {
         // Trigger new page so first tab focuses skip links
         const prevPageLocation = history[history.length - 2];
         const currentPageLocation = currentUrl;
+
         if (this.newPage(prevPageLocation, currentPageLocation)) {
           this.tabChangeService.triggerNewPage();
         }
@@ -89,45 +114,28 @@ export class MysteryService implements OnDestroy {
           )
         ) {
           const targetPage = +router.parseUrl(currentUrl).queryParams.page || 1;
-          // Different page or coming from different route
-          if (
-            this.startPage !== targetPage ||
-            !history[history.length - 2]?.includes('/results')
-          ) {
-            scrollToTop();
-          }
           this.startPage = targetPage;
 
           // Similar to /results but for /funding-calls
-        } else if (currentUrl.includes('/funding-calls')) {
-          const targetPage = +router.parseUrl(currentUrl).queryParams.page || 1;
-          // Different page or coming from different route
-          if (
-            this.startPage !== targetPage ||
-            !history[history.length - 2]?.includes('/funding-calls')
-          ) {
-            scrollToTop();
+        } else if (
+          currentUrl.includes('/results') &&
+          resultPages.some((item) =>
+            currentUrl.includes(`/${item.slice(0, -1)}/`)
+          )
+        ) {
+          if (isPlatformBrowser(this.platformId)) {
+            // Navigated from results page
+            if (this.isResultPage(history[history.length - 2])) {
+              this.resultsPageScrollPositionY = viewportScroller.getScrollPosition()[1];
+              viewportScroller.scrollToPosition([0, 0]);
+            }
           }
-          this.startPage = targetPage;
-        } else if (currentUrl.includes('/science-research-figures')) {
-          // scroll to top only in single figure view
-          if (!history[history.length - 2]?.includes('figures/s')) {
-            scrollToTop();
-          }
-          if (!currentUrl.includes('filter')) {
-            scrollToTop();
-          }
-        } else {
-          scrollToTop();
         }
       });
-
-    // console.log("XXX Adding global icons");
-    // Add global icons
-    library.addIcons(faExternalLinkAlt as any, faInfoCircle as any);
   }
 
   ngOnDestroy(): void {
     this.routeSub?.unsubscribe();
+    this.isContentFetched?.unsubscribe();
   }
 }
