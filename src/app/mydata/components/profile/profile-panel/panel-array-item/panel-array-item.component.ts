4de95936847ed614c0db7c0292c0be7ec72ac517
konolak <kari.konola@csc.fi>
2025-05-21 14:16:06 +0300

    ui renewal in progress. sort publications by year and misc style changes

--- src/app/mydata/components/profile/profile-panel/panel-array-item/panel-array-item.component.ts
+++ src/app/mydata/components/profile/profile-panel/panel-array-item/panel-array-item.component.ts
@@ -11,31 +11,34 @@ import { AppSettingsService } from '@shared/services/app-settings.service';
 import { ParseDatePipe } from '../../../../pipes/parse-date.pipe';
 import { GetLocalizedValuesPipe } from '../../../../pipes/getLocalizedValues.pipe';
 import { ActivityItemComponent } from '../activity-item/activity-item.component';
-import { NgIf, NgSwitch, NgSwitchCase, NgFor, NgTemplateOutlet, NgSwitchDefault } from '@angular/common';
+import { NgIf, NgSwitch, NgSwitchCase, NgFor, NgTemplateOutlet, NgSwitchDefault, NgClass } from '@angular/common';
 
 @Component({
     selector: 'app-panel-array-item',
     templateUrl: './panel-array-item.component.html',
+    styleUrls: ['./panel-array-item.component.scss'],
     standalone: true,
-    imports: [
-        NgIf,
-        NgSwitch,
-        NgSwitchCase,
-        NgFor,
-        ActivityItemComponent,
-        NgTemplateOutlet,
-        NgSwitchDefault,
-        GetLocalizedValuesPipe,
-        ParseDatePipe,
-    ],
+  imports: [
+    NgIf,
+    NgSwitch,
+    NgSwitchCase,
+    NgFor,
+    ActivityItemComponent,
+    NgTemplateOutlet,
+    NgSwitchDefault,
+    GetLocalizedValuesPipe,
+    ParseDatePipe,
+    NgClass
+  ]
 })
 export class PanelArrayItemComponent implements OnInit {
   @Input() set item(item: any) {
     this._item = item;
   }
-  @Input() fieldType: string;
+  @Input() fieldType: number;
   @Input() localized: boolean;
   @Input() summary: boolean;
+  hasOpenScienceTag = false;
 
   fieldTypes = FieldTypes;
   _item: any;
@@ -47,5 +50,13 @@ export class PanelArrayItemComponent implements OnInit {
     this.locale = this.appSettingsService.capitalizedLocale;
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this._item?.dataSources?.forEach(ds => {
+      console.log('ds', ds);
+      if (ds.registeredDataSource === 'Tiedejatutkimus.fi') {
+        console.log('!!!!! ttv', this._item);
+      }
+      }
+    );
+  }
 }
