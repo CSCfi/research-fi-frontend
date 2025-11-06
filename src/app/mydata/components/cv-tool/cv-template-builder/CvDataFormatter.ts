5515406c9c1913a66dae97d1e818540ad1f1b660
konolak <kari.konola@csc.fi>
2025-11-06 14:59:10 +0200

    fix cv tool time formatting and add missing position name

--- src/app/mydata/components/cv-tool/cv-template-builder/CvDataFormatter.ts
+++ src/app/mydata/components/cv-tool/cv-template-builder/CvDataFormatter.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
 import { checkTranslation as checkTranslation} from '@portal/models/person/profiletool-person-adapter';
-import { FieldTypes as fieldTypes } from '@mydata/constants/fieldTypes';
 import { cloneDeep } from 'lodash-es';
+import { GroupTypes } from '@mydata/constants/groupTypes';
+import * as translations from '@mydata/components/cv-tool/cv-template-builder/CvTranslations';
 
 export interface cvDataFormatted {
   firstNames: string;
@@ -31,13 +31,115 @@ function getDateNow() {
   return [day, month, year].join('.');
 }
 
+
+  const groupTypes = GroupTypes;
+  function formatAndSortTimeSpan(data: any, dataType: string, langCode: string): unknown {
+
+    const presentLocalization =  translations.getTranslation(langCode, 'present_term');
+
+    if (data) {
+      let items = cloneDeep(data);
+
+      if (dataType === groupTypes.funding) {
+        items = items.map(item => {
+          item.startDate = {year: item.startYear ? item.startYear : 0};
+          item.endDate = {year: item.endYear ? item.endYear : 0};
+          return item;
+        });
+      }
+
+      items = items.sort(customSort);
+
+      items = items.map(item => {
+        // Show single year
+        if (item.startDate.year === item.endDate.year) {
+          if (item.endDate.year === 0) {
+            item.timing = '';
+          } else {
+            item.timing = item.startDate.year.toString();
+          }
+        }
+        // Start date missing
+        else if (item.startDate.year === 0) {
+          if (item.endDate.year > 0) {
+            item.timing = item.endDate.year.toString();
+          }
+          // Start and end date missing
+          else {
+            item.timing = '';
+          }
+          // End date missing
+        } else if (item.endDate.year === 0) {
+          item.timing = item.startDate.year.toString();
+          if (dataType === groupTypes.activitiesAndRewards || dataType === groupTypes.education) {
+            item.timing = item.timing + ' - ' + presentLocalization;
+          }
+        }
+        // Regular case
+        else {
+          let startDate = '';
+          let endDate = '';
+          if (item.startDate.month) {
+            startDate = item.startDate.month + '/' + item.startDate.year;
+          } else {
+            startDate = item.startDate.year.toString();
+          }
+          if (item.endDate.month) {
+            endDate = item.endDate.month + '/' + item.endDate.year;
+          } else {
+            endDate = item.endDate.year.toString();
+          }
+          item.timing = startDate + ' - ' + endDate;
+        }
+        return item;
+      });
+
+      // Sort items with empty timing to last
+      const timingExists = [];
+      const noTiming = [];
+      items.forEach(item => {
+        if (item.timing === '') {
+          noTiming.push(item);
+        }
+        else {
+          timingExists.push(item);
+        }
+      });
+      items = timingExists.concat(noTiming);
+      return items ? items : [];
+    }
+  }
+
+
+function customSort(a, b) {
+  // Return comparison in reversed order
+  if (a.endDate.year === 0 && b.endDate.year === 0) {
+    // Both are missing end years, so sort by start year
+    return a.startDate.year > b.startDate.year ? -1 : a.startDate.year < b.startDate.year ? 1 : 0;
+  }
+  if (a.endDate.year > 0 && b.endDate.year > 0) {
+    // Both have end years
+    return a.endDate.year > b.endDate.year ? -1 : a.endDate.year < b.endDate.year ? 1 : 0;
+  }
+  // Other end year is present
+  else if (a.endDate.year > 0) {
+    // B is "present day" (bigger)
+    return 1;
+  } // A is "present day" (bigger)
+  else return -1;
+}
+
 export function formatCvData(lang: string, profileData, orcid: string, filterVisible: boolean) {
   console.log('formatCvData', JSON.stringify(profileData), filterVisible);
 
   let visibleNames = profileData[0].fields[0].items.filter(item => filterVisible ? item.itemMeta.show === true : item);
 
   let affiliations = profileData[2].fields[0].items.filter(item => filterVisible ? item.itemMeta.show === true : item);
-  let parsedAffiliations = affiliations.map(item => {
+
+  let formattedAffiliations = formatAndSortTimeSpan(affiliations, groupTypes.affiliation, lang);
+
+  // @ts-ignore
+  let parsedAffiliations = formattedAffiliations.map(item => {
     item.name = checkTranslation('name', item, lang) ?? '';
     item.organizationName = checkTranslation('organizationName', item, lang) ?? '';
     return item;
@@ -56,8 +158,11 @@ export function formatCvData(lang: string, profileData, orcid: string, filterVis
   });
 
   let parsedEducation = profileData[3].fields[0].items.filter(item => filterVisible ? item.itemMeta.show === true : item);
-  console.log(parsedEducation, profileData[3]);
-  parsedEducation = parsedEducation.map(item => {
+
+  let formattedEducation = formatAndSortTimeSpan(parsedEducation, groupTypes.education, lang);
+
+  // @ts-ignore
+  parsedEducation = formattedEducation.map(item => {
     item.name = checkTranslation('name', item, lang) ?? '';
     return item;
   });
@@ -79,6 +184,8 @@ export function formatCvData(lang: string, profileData, orcid: string, filterVis
 
 
   let parsedActivities = profileData[7].fields[0].items.filter(item => filterVisible ? item.itemMeta.show === true : item);
+
+  parsedActivities = formatAndSortTimeSpan(parsedActivities, groupTypes.activitiesAndRewards, lang);
 
   let formattedData: cvDataFormatted = {
     awardsAndHonors: [],
