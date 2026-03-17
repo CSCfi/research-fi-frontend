0c5d8439e709d4bbb92cfa6d74c2778750e7dea6
konolak <kari.konola@csc.fi>
2026-03-17 16:44:12 +0200

    footer renewal. work stashed

---
+++ src/app/layout/footer-renewed/footer-renewed.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-footer-renewed',
+  templateUrl: './footer-renewed.component.html',
+  styleUrls: ['./footer-renewed.component.scss']
+})
+export class FooterRenewedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
