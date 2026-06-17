27c2aafdcb6b89df60660ca8ce5a2b1189e39ff2
konolak <kari.konola@csc.fi>
2026-06-17 18:04:37 +0300

    restore ows pie to single dataset side bar

--- src/app/portal/components/single/single-dataset/single-dataset.component.ts
+++ src/app/portal/components/single/single-dataset/single-dataset.component.ts
@@ -37,6 +37,7 @@ import { BreadcrumbComponent } from '../../breadcrumb/breadcrumb.component';
 import { NgClass } from '@angular/common';
 import { SearchBarComponent } from '../../search-bar/search-bar.component';
 import { SvgSpritesComponent } from '@shared/components/svg-sprites/svg-sprites.component';
+import { OwsRelatedPieChartComponent } from '@shared/components/ows-related-pie-chart/ows-related-pie-chart.component';
 
 @Component({
     selector: 'app-single-dataset',
@@ -60,8 +61,9 @@ import { SvgSpritesComponent } from '@shared/components/svg-sprites/svg-sprites.
     ShareComponent,
     MatProgressSpinner,
     SafeUrlPipe,
-    SvgSpritesComponent
-]
+    SvgSpritesComponent,
+    OwsRelatedPieChartComponent
+  ]
 })
 export class SingleDatasetComponent implements OnInit {
   public singleId: any;
